ccc10f50c4eb6951a770a4526a00f14f6065e474
10546007 <10546007@ntub.edu.tw>
2019-05-07 19:40:52 +0800

    add list() function to articleList.ejs

--- routes/articleList.js
+++ routes/articleList.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+const article = require('./utility/article');
 //接收GET請求
 router.get('/', function(req, res, next) {
-    res.render('articleList', { title: 'Express' });
-  });
-
+  article.list().then(data => {
+      if(data==null){
+          res.render('error');  //導向錯誤頁面
+      }else if(data.length > 0){
+          res.render('articleList', {items:data});  //將資料傳給顯示頁面
+      }else{
+          res.render('notFound');  //導向找不到頁面
+      }  
+  })
+});
 module.exports = router;
