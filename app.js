5ecf9a8a9db97d98706a11e8c18859952e8ffdfc
10546007 <10546007@ntub.edu.tw>
2019-05-17 18:55:33 +0800

    add utility/signIn.js and express-session and userLogin.js

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+var session = require('express-session');
+
 // --------------  My routes--------------- 
 var collection_recommend_Router = require('./routes/collectionRecommend');
 var collection_article_Router = require('./routes/collectionArticle');
@@ -15,6 +18,8 @@ var articleRouter = require('./routes/article');
 var signInRouter = require('./routes/signIn');
 var signUpRouter = require('./routes/signUp');
 var signUpAddRouter = require('./routes/signUp_add');
+var userLoginRouter = require('./routes/userLogin');
+
 // ---------------  My four Class -------------------
 var articleMovieRouter = require('./routes/articleMovie');
 var articleMusicRouter = require('./routes/articleMusic');
@@ -31,7 +36,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(session({secret: 'mysecret', cookie: { maxAge: 60000 }}));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/article', articleRouter);
@@ -43,6 +48,7 @@ app.use('/articleList',articleListRouter);
 app.use('/signIn',signInRouter);
 app.use('/signUp',signUpRouter);
 app.use('/signUp/add',signUpAddRouter);
+app.use('/login',userLoginRouter);
 // -------------- My use Four Class ----------------
 app.use('/articleList/articleMovie',articleMovieRouter);
 app.use('/articleList/articleMusic',articleMusicRouter);
---
+++ routes/userLogin.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+//增加引用函式
+const signIn = require('./utility/signIn');
+
+//接收POST請求
+router.post('/', function (req, res, next) {
+    var memID = req.body.memID;                 //取得帳號
+    var memPass = req.body.memPass;     //取得密碼
+    console.log(memID);
+    signIn.login(memID, memPass).then(d => {
+        if (d == null) {
+            req.session.memID = null;
+            req.session.memPass = null;
+            res.write('<head><meta charset="utf-8"/></head>');
+            res.end('<script> alert("登入失敗，尚未註冊帳號。"); history.back();</script>');
+
+        } else {
+            req.session.memID = d.memID;
+            req.session.memPass = d.memPass;
+            res.render('signIn', { name: d.memID });   //導向使用者
+        }
+    })
+});
+
+module.exports = router;
