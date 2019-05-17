1bc27d8f3cc1c89cf937e94ae8c1730c5597d4d0
10546004 <10546004@ntub.edu.tw>
2019-05-17 11:55:20 +0800

    search form

--- scripts/header.js
+++ scripts/header.js
@@ -1,6 +1,9 @@
 header = '    <div class="header">\
 <nav class="navbar navbar-expand-lg navbar-light fixed-top ">\
-    <a class="navbar-brand" href="#">Navbar</a>\
+    <a class="nav-link" href="#">\
+    <img class="nav-item" href="#" src="imgs/logo.png" width="35px" height="35px" alt="">\
+       <a class="navTitle">文藝富心</a>\
+</a>\
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">\
         <span class="navbar-toggler-icon"></span>\
     </button>\
@@ -18,7 +21,7 @@ header = '    <div class="header">\
                 <a class="nav-link" href="articleList.html">文章列表</a>\
             </li>\
             <li class="nav-item ">\
-                <a class="nav-link" href="collection-recommend.html">我的收藏</a>\
+                <a class="nav-link" href="collectionRecommend.html">我的收藏</a>\
             </li>\
             <li class="nav-item">\
                 <a class="nav-link" href="signIn.html">會員</a>\
