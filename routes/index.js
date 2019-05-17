241d422f634e93bfa30e2639da40339c90348057
10546007 <10546007@ntub.edu.tw>
2019-05-17 18:54:02 +0800

    modify index.js and utility/ariticle.js async issue

--- routes/index.js
+++ routes/index.js
@@ -4,65 +4,14 @@ var router = express.Router();
 const article = require('./utility/article');
 var moment = require('moment');
 
-var mydata = []; //用來取得data 並回傳給index.ejs
-
-//依照
-function myfunction(hotArticle, i) {
-    article.getHotArticle(hotArticle[i]).then(data => {
-        if (data == null) {
-            res.render('error');  //導向錯誤頁面
-        } else if (data.length > 0) {
-            data[0].artiDateTime = moment(data[0].artiDateTime).format("YYYY-MM-DD HH:mm:ss");
-            mydata.push(data); //將data資料存到mydata
-        } else {
-            res.render('notFound');  //導向找不到頁面
-        }
-    })
-}
-
-
 //接收GET請求
 router.get('/', function (req, res, next) {
-    var articleLikeCount = [] //存放 articleLike表中裡面的 artiNum欄位
-    var max = 0; // 尋找在articleLike表中 出現最多次的artiNum
-    var times = 0; //作為判斷是否取得三篇文章的開關
-    var hotArticle = [];  //存放前三名熱門文章
-
-    article.getArticleNum().then(data => { //取得artiNum
+    article.getHotArticle().then(data => { //取得artiNum
+        // console.log(data);
         if (data == null) {
             res.render('error');  //導向錯誤頁面
         } else if (data.length > 0) {
-            //初始化陣列 
-            for (let i = 0; i <= data.length; i++) {
-                articleLikeCount[i] = 0;
-            }
-            //將取得的文章編號做計算 (articleLikeCount陣列維度代表第幾篇文章 裡面的值代表共有幾個讚)
-            for (let i = 0; i < data.length; i++) {
-                articleLikeCount[data[i].artiNum] += 1;
-            }
-            // console.log(articleLikeCount);
-            //尋找前三名
-            max = Math.max(...articleLikeCount);  //取得最大值
-            while (times < 3) {
-                for (let i = 1; i <= articleLikeCount.length; i++) {
-                    if (times == 3) break;
-                    if (articleLikeCount[i] == max) {
-                        hotArticle[times] = i;
-                        times += 1;
-                    }
-                }
-                max -= 1;
-            }
-            // 撈前三篇文章的資訊
-            for (let i = 0; i < 3; i++) {
-                myfunction(hotArticle, i);
-            }
-            //解決非同步問題，迴圈執行完後再執行
-            setTimeout(function () {
-                res.render('index', { items: mydata });
-                // console.log("RenData", mydata);
-                mydata = []; //初始化
-            }, 3000)
+            res.render('index', { items: data });
         } else {
             res.render('notFound');  //導向找不到頁面
         }
--- routes/utility/article.js
+++ routes/utility/article.js
@@ -2,134 +2,161 @@
 
 //引用操作資料庫的物件
 const sql = require('./asyncDB');
-
+var moment = require('moment');
 //------------------------------------------
 // get article list
 //------------------------------------------
-var getArticleList = async function(){
-    var result=[];
-	
+var getArticleList = async function () {
+    var result = [];
+
     await sql('SELECT * FROM article')
-        .then((data) => {            
-            result = data.rows;  
+        .then((data) => {
+            result = data.rows;
         }, (error) => {
             result = null;
         });
-		
+
     return result;
 }
 //---------  getOneArticle() -------------
-var getOneArticle = async function(artiNum){
-    var result={};
-    
+var getOneArticle = async function (artiNum) {
+    var result = {};
+
     await sql('SELECT * FROM article WHERE "artiNum" = $1', [artiNum])
         .then((data) => {
-            if(data.rows.length > 0){
-                result = data.rows[0];   
-            }else{
+            if (data.rows.length > 0) {
+                result = data.rows[0];
+            } else {
                 result = -1;
-            }    
+            }
         }, (error) => {
             result = null;
         });
-    
-		
+
+
     return result;
 }
 //---------  getArticleMessage() -------------
-var getArticleMessage = async function(artiNum){
-    var result=[];
-	
+var getArticleMessage = async function (artiNum) {
+    var result = [];
+
     await sql('SELECT * FROM "articleMessage" WHERE "artiNum" = $1', [artiNum])
-        .then((data) => {            
-            result = data.rows;  
+        .then((data) => {
+            result = data.rows;
         }, (error) => {
             result = null;
         });
-		
+
     return result;
 }
 // ==================  Four Class  (start)=========================
 //---------  getClassMovie() -------------
-var getClassMovie = async function(){
-    var result=[];
-	
+var getClassMovie = async function () {
+    var result = [];
+
     await sql('SELECT * FROM "article" WHERE "artiClass" = $1', ['movie'])
-        .then((data) => {            
-            result = data.rows;  
+        .then((data) => {
+            result = data.rows;
         }, (error) => {
             result = null;
         });
-		
+
     return result;
 }
 //---------  getClassMusic() -------------
-var getClassMusic = async function(){
-    var result=[];
-	
+var getClassMusic = async function () {
+    var result = [];
+
     await sql('SELECT * FROM "article" WHERE "artiClass" = $1', ['music'])
-        .then((data) => {            
-            result = data.rows;  
+        .then((data) => {
+            result = data.rows;
         }, (error) => {
             result = null;
         });
-		
+
     return result;
 }
 //---------  getClassBook() -------------
-var getClassBook = async function(){
-    var result=[];
-	
+var getClassBook = async function () {
+    var result = [];
+
     await sql('SELECT * FROM "article" WHERE "artiClass" = $1', ['book'])
-        .then((data) => {            
-            result = data.rows;  
+        .then((data) => {
+            result = data.rows;
         }, (error) => {
             result = null;
         });
-		
+
     return result;
 }
 //---------  getClassExhibition() -------------
-var getClassExhibition = async function(){
-    var result=[];
-	
+var getClassExhibition = async function () {
+    var result = [];
+
     await sql('SELECT * FROM "article" WHERE "artiClass" = $1', ['exhibition'])
-        .then((data) => {            
-            result = data.rows;  
+        .then((data) => {
+            result = data.rows;
         }, (error) => {
             result = null;
         });
-		
+
     return result;
 }
 // ==================  Four Class  (end)=========================
-// ==================  get hot article (start)=========================
 //---------  getArticleNum() -------------
-var getArticleNum = async function(){
+var getHotArticle = async function () {
     var result = [];
+    var mydata = [];
+    var articleLikeCount = [] //存放 articleLike表中裡面的 artiNum欄位
+    var max = 0; // 尋找在articleLike表中 出現最多次的artiNum
+    var times = 0; //作為判斷是否取得三篇文章的開關
+    var hotArticle = [];  //存放前三名熱門文章
+
     await sql('select "articleLike"."artiNum" from "articleLike"')
-        .then((data) => {            
-            result = data.rows;  
+        .then((data) => {
+            result = data.rows;
+            //初始化陣列 
+            for (let i = 0; i <= result.length; i++) {
+                articleLikeCount[i] = 0;
+            }
+            //將取得的文章編號做計算 (articleLikeCount陣列維度代表第幾篇文章 裡面的值代表共有幾個讚)
+            for (let i = 0; i < result.length; i++) {
+                articleLikeCount[result[i].artiNum] += 1;
+            }
+            // console.log(articleLikeCount);
+            //尋找前三名
+            max = Math.max(...articleLikeCount);  //取得最大值
+            while (times < 3) {
+                for (let i = 1; i <= articleLikeCount.length; i++) {
+                    if (times == 3) break;
+                    if (articleLikeCount[i] == max) {
+                        hotArticle[times] = i;
+                        times += 1;
+                    }
+                }
+                max -= 1;
+            }
         }, (error) => {
             result = null;
         });
-		
-    return result;
+    // 撈前三篇文章的資訊
+    for (let i = 0; i < 3; i++) {
+        await sql('select * from "article" where "artiNum" = $1 ', [hotArticle[i]])
+            .then((data) => {
+                data.rows[0].artiDateTime = moment(data.rows[0].artiDateTime).format("YYYY-MM-DD HH:mm:ss");
+                mydata[i] = data.rows;
+            }, (error) => {
+                mydata = null;
+            });
+    }
+
+    // console.log("test=",test[0]);
+    return mydata;
 }
-//---------  getHotArticle() -------------
-var getHotArticle = async function(artiNum){
-    var result = [];
-    await sql('select * from "article" where "artiNum" = $1 ',[artiNum])
-        .then((data) => {            
-            result = data.rows;  
-        }, (error) => {
-            result = null;
-        });
-		
-    return result;
-}
-// ==================  get hot article (end)=========================
+
 //匯出
-module.exports = {getArticleList,getOneArticle,getArticleMessage,
-    getClassMovie,getClassMusic,getClassBook,getClassExhibition,
-    getArticleNum,getHotArticle};
+module.exports = {
+    getArticleList, getOneArticle, getArticleMessage,
+    getClassMovie, getClassMusic, getClassBook, getClassExhibition,
+    getHotArticle
+};
