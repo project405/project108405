65e3e3f508f2cc2a129c46513db3be601ec85459
10546004 <10546004@ntub.edu.tw>
2019-06-24 22:27:46 +0800

    formatted text

--- scripts/active.js
+++ scripts/active.js
@@ -82,8 +82,21 @@ $(document).ready(function() {
 
 });
 
+//格式化可編輯的div裡的文字
+document.querySelector('div[contenteditable="true"]').addEventListener("paste", function(e) {
+    e.preventDefault();
+    var text = e.clipboardData.getData("text/plain");
+    document.execCommand("insertHTML", false, text);
+});
 
 
+[].forEach.call(document.querySelectorAll('div[contenteditable="true"]'), function(el) {
+    el.addEventListener('paste', function(e) {
+        e.preventDefault();
+        var text = e.clipboardData.getData("text/plain");
+        document.execCommand("insertHTML", false, text);
+    }, false);
+});
 
 // (function($) {
 //     $.fn.extend({
