ef20dd0121385f5e3cc6ff24d5e581da1bd50f0f
weiting <canithatcatchu0@gmail.com>
2019-06-27 15:36:52 +0800

    input ontline -> dashed 3px

--- scripts/active.js
+++ scripts/active.js
@@ -1,31 +1,37 @@
-// 隨著留言增加height增加
-
-jQuery.fn.extend({
-    autoHeight: function() {
-        return this.each(function() {
-            var $this = jQuery(this);
-            if (!$this.attr('_initAdjustHeight')) {
-                $this.attr('_initAdjustHeight', $this.outerHeight());
-            }
-            _adjustH(this).on('input', function() {
-                _adjustH(this);
-            });
-        });
-        /**
-         * 重置高度 
-         * @param {Object} elem
-         */
-        function _adjustH(elem) {
-            var $obj = jQuery(elem);
-            return $obj.css({ height: $obj.attr('_initAdjustHeight'), 'overflow-y': 'hidden' })
-                .height(elem.scrollHeight);
-        }
-    }
-});
-// 使用
+// 點擊其他地方收navbar
 $(function() {
-    $('#textarea').autoHeight();
+    $(document).click(function(event) {
+        $('.navbar-collapse').collapse('hide');
+    });
 });
+
+// 隨著留言增加height增加
+// jQuery.fn.extend({
+//     autoHeight: function() {
+//         return this.each(function() {
+//             var $this = jQuery(this);
+//             if (!$this.attr('_initAdjustHeight')) {
+//                 $this.attr('_initAdjustHeight', $this.outerHeight());
+//             }
+//             _adjustH(this).on('input', function() {
+//                 _adjustH(this);
+//             });
+//         });
+//         /**
+//          * 重置高度 
+//          * @param {Object} elem
+//          */
+//         function _adjustH(elem) {
+//             var $obj = jQuery(elem);
+//             return $obj.css({ height: $obj.attr('_initAdjustHeight'), 'overflow-y': 'hidden' })
+//                 .height(elem.scrollHeight);
+//         }
+//     }
+// });
+// 使用
+// $(function() {
+//     $('#textarea').autoHeight();
+// });
 
 
 //gotop
--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,8 @@ $(document).ready(function() {
         if (wn > 120) {
             $(".navbar").css("background", "#2D4F41");
             $(".navbar").css("opacity", "0.85");
+            $(".navbar").css("transition", "0.5s ease-in-out");
+            // $(".navbar").css("box-shadow", " 0 5px 15px rgba(0,0,0,0.3)");
 
         } else {
             $(".navbar").css("background", "");
