783ac3e3d186907291d871fc2e06c9e474405e1b
10546004 <10546004@ntub.edu.tw>
2019-07-06 20:45:30 +0800

    recommendform

--- scripts/form.js
+++ scripts/form.js
@@ -3,13 +3,22 @@ $(window).load(function() {
 })
 
 function styleselect() {
-    if (document.getElementById('globalstyleselect').value == "1") {
-        $("#stylediv").html('<b>Boca Style</b>');
+
+    if (document.getElementById('inputState').value == "1") {
+        $("#infoForm").html('名稱：<input type="text" placeholder="電影名稱"><br>上映日期：<input type="date"><br>評分一：<input type="text" placeholder="第一平台評分"><br>評分二：<input type="text" placeholder="第二平台評分"><br>導演：<input type="text" placeholder="導演"><br>演員：<input type="text" placeholder="演員"><br>分級：<select col-sm-3"><option value="1">普遍級</option><option value="2">保護級</option><option value="3">輔導級</option><option value="4">限制級</option></select><br>發布日期：<input type="date">');
     }
-    if (document.getElementById('globalstyleselect').value == "2") {
-        $("#stylediv").html('<b>Bella Style</b>');
+    if (document.getElementById('inputState').value == "2") {
+        $("#infoForm").html('歌名：<input type="text" placeholder="歌名"><br>歌詞：<input type="text" placeholder="歌詞"><br>歌手：<input type="text" placeholder="歌手"><br>專輯：<input type="text" placeholder="專輯"><br>發布日期：<input type="date">');
     }
-}
-if (document.getElementById('globalstyleselect').value == "3") {
-    $("#stylediv").html('<b>Terra Style</b>');
+
+    if (document.getElementById('inputState').value == "3") {
+        $("#infoForm").html('名稱：<input type="text" placeholder="主題名稱"><br>創作者：<input type="text" placeholder="創作者"><br>單位：<input type="text" placeholder="主辦單位"><br>地點：<input type="text" placeholder="地點"><p>價格：<textarea type="text" placeholder="價格資訊"></textarea></p>開始日：<input type=datetime-local><br>截止日：<input type=datetime-local><br>發布日期：<input type="date">');
+
+    }
+
+    if (document.getElementById('inputState').value == "4") {
+        $("#infoForm").html('ISBN：<input type="text" placeholder="ISBN"><br>書名：<input type="text" placeholder="書名"><br>作者：<input type="text" placeholder="作者"><br>書評一：<input type="text" placeholder="第一平台書評"><br>書評二：<input type="text" placeholder="第二平台書評"><br>出版社：<input type="text" placeholder="出版社"><br>發布日期：<input type="date">');
+
+    }
+
 }
--- scripts/post.js
+++ scripts/post.js
@@ -88,3 +88,13 @@ $(function() {
         // });
     });
 });
+
+$('div[contenteditable]').keydown(function(e) {
+    // trap the return key being pressed
+    if (e.keyCode === 13) {
+        // insert 2 br tags (if only one br tag is inserted the cursor won't go to the next line)
+        document.execCommand('insertHTML', false, '<br><br>');
+        // prevent the default behaviour of return key pressed
+        return false;
+    }
+});
