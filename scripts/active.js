dece322a1a5fd2daed28fe2faedeec99ca48d147
10546004 <10546004@ntub.edu.tw>
2019-07-19 11:24:47 +0800

    adjust

--- scripts/active.js
+++ scripts/active.js
@@ -1,15 +1,11 @@
-// 點擊其他地方收navbar
-$(function() {
-    $(document).click(function(event) {
-        $('.navbar-collapse').collapse('hide');
-    });
-    $('.navbar-collapse').click(function(event) {
-        event.stopPropagation();
-    });
-});
+$(document).ready(function() {
+    // $(document).click(function(event) {
+    //     $('.navbar-collapse').collapse('hide');
+    // });
+    // $('.navbar-collapse').click(function(event) {
+    //     event.stopPropagation();
+    // });
 
-// tag div css
-$(function() {
     $("input").focus(function() {
         $("div.tag").css("box-shadow", "0 5px 15px rgba(0, 0, 0, 0.3)");
         $("div.tag").css("transition", "0.3s ease-in-out");
@@ -17,68 +13,32 @@ $(function() {
     $("input").blur(function() {
         $("div.tag").css("box-shadow", "none");
     });
-});
 
-// 隨著留言增加height增加
-// jQuery.fn.extend({
-//     autoHeight: function() {
-//         return this.each(function() {
-//             var $this = jQuery(this);
-//             if (!$this.attr('_initAdjustHeight')) {
-//                 $this.attr('_initAdjustHeight', $this.outerHeight());
-//             }
-//             _adjustH(this).on('input', function() {
-//                 _adjustH(this);
-//             });
-//         });
-//         /**
-//          * 重置高度 
-//          * @param {Object} elem
-//          */
-//         function _adjustH(elem) {
-//             var $obj = jQuery(elem);
-//             return $obj.css({ height: $obj.attr('_initAdjustHeight'), 'overflow-y': 'hidden' })
-//                 .height(elem.scrollHeight);
-//         }
-//     }
-// });
-// 使用
-// $(function() {
-//     $('#textarea').autoHeight();
-// });
+    $(".wrapText").each(function() {
 
-//if word too long 
+        //取得內容
+        var str = $(this).html();
 
-$(".wrapText").each(function() {
+        //截取内容75字
+        var subStr = str.substring(0, 100);
 
-    //取得內容
-    var str = $(this).html();
+        //如果長度大於75就添加省略號否則就填空
+        var data = subStr + (str.length > 75 ? '...' : '');
+        $(this).html(data);
+    });
+    $(".wrapBigHotSectionText").each(function() {
 
-    //截取内容75字
-    var subStr = str.substring(0, 100);
+        //取得內容
+        var str = $(this).html();
 
-    //如果長度大於75就添加省略號否則就填空
-    var data = subStr + (str.length > 75 ? '...' : '');
-    $(this).html(data);
-});
+        //截取内容75字
+        var subStr = str.substring(0, 200);
 
+        //如果長度大於75就添加省略號否則就填空
+        var data = subStr + (str.length > 200 ? '...' : '');
+        $(this).html(data);
+    });
 
-$(".wrapBigHotSectionText").each(function() {
-
-    //取得內容
-    var str = $(this).html();
-
-    //截取内容75字
-    var subStr = str.substring(0, 200);
-
-    //如果長度大於75就添加省略號否則就填空
-    var data = subStr + (str.length > 200 ? '...' : '');
-    $(this).html(data);
-});
-
-
-//gotop
-$(function() {
     /* 按下GoTop按鈕時的事件 */
     $('#gotop').click(function() {
         $('html,body').animate({ scrollTop: 0 }, 'slow'); /* 返回到最頂上 */
@@ -92,17 +52,17 @@ $(function() {
             $('#gotop').fadeOut();
         };
     });
-});
 
-//bottom
-$("#goBottom").click(function() {
-    $('html,body').animate({ scrollTop: $('#reply').offset().top }, 1000);
-});
-
+    //bottom
+    $("#goBottom").click(function() {
+        $('html,body').animate({ scrollTop: $('#reply').offset().top }, 1000);
+    });
 
 
-//goDown
 
-$('#goDown').click(function() {
-    $('html,body').animate({ scrollTop: $('#block').offset().top }, 800);
+    //goDown
+
+    $('#goDown').click(function() {
+        $('html,body').animate({ scrollTop: $('#block').offset().top }, 800);
+    });
 });
