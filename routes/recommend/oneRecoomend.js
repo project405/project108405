2f522b0702640e911511534410e4dc18fd5e3f96
chin_ling <chin_ling@gss.com.tw>
2019-08-10 22:27:32 +0800

    modify tag error

--- routes/recommend/oneRecoomend.js
+++ routes/recommend/oneRecoomend.js
@@ -7,16 +7,15 @@ router.get('/:recomNum', async function (req, res, next) {
     var recomNum = req.params.recomNum;   //取出參數
     var memID = req.session.memID;
     recommend.getOneRecommend(recomNum, memID).then(data => {
-        // 測試data
-        // for (var i = 0; i < data[0].length; i++) {
-        //     if (data[0][i].recomCont.match("\\:imgLocation") != null) {
-        //         for (var j = 0; j < data[11][0].length; j++) {
-        //             // artiCont = artiCont.replace("\\:imgLocation", "<div class='wrapperCard card-img-top' style='background-image: url(/userImg/" + req.files[i].filename + "'); border-radius:8px; '></div>");
-        //             data[0][i].recomCont = data[0][i].recomCont.replace("\\:imgLocation", "<div class='wrapperCard card-img-top'><img src='/userImg/" + data[11][0][j].imgName + "' style='max-height: 450px; max-width: 70%; cursor: pointer; border-radius: 12px; padding: 0.35em; ' ></div>");
-        //             // console.log("data[", 11, "][", j, "]=", data[11][0][j]);
-        //         }
-        //     }
-        // }
+
+        // 將圖片字串替代成圖片
+        for (var i = 0; i < data[0].length; i++) {
+            if (data[0][i].recomCont.match("\\:imgLocation") != null) {
+                for (var j = 0; j < data[3].length; j++) {
+                    data[0][i].recomCont = data[0][i].recomCont.replace("\\:imgLocation", "<div class='wrapperCard card-img-top'><img src='/userImg/" + data[3][j].imgName + "' style='max-height: 450px; max-width: 70%; cursor: pointer; border-radius: 12px; padding: 0.35em; ' ></div>");
+                }
+            }
+        }
 
         if (data == null) {
             res.render('error');  //導向錯誤頁面
