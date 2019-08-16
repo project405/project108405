790e817be9ea948dec9a4c9547ee55955b19d7f8
project405 <10546017@ntub.edu.tw>
2019-08-17 05:26:33 +0800

    myApp

--- index.js
+++ index.js
@@ -144,7 +144,7 @@ bot.on('message', function(event) {
                 }else if(data == -9){                    
                     event.reply('執行錯誤');
                 }else{
-                    console.log(data.result[1]);
+                    console.log(data);
                     // event.reply([
                     //     {'type':'text', 'text':data[0].recomHead},
                     //     {'type':'text', 'text':data[0].recomCont},
