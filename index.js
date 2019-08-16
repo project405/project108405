a49d9e21c35734a29331291fe2876a7a243a8d75
project405 <10546017@ntub.edu.tw>
2019-08-17 06:04:48 +0800

    myApp

--- index.js
+++ index.js
@@ -137,15 +137,15 @@ bot.on('message', function(event) {
     if (text == "熱門文章"){
         index.getIndexData(text).then(data => {  
             
-            console.log(data[1][0]);
-            console.log(data[1][1]);
-            console.log(data[1][2]);
+            // console.log(data[1][0]);
+            // console.log(data[1][1]);
+            // console.log(data[1][2]);
             
-            // event.reply([
-            //     {'type':'text', 'text':data[1][0]},
-            //     {'type':'text', 'text':data[1][1]},
-            //     {'type':'text', 'text':data[1][2]}
-            // ]);   
+            event.reply([
+                {'type':'text', 'text':data[1][0]},
+                {'type':'text', 'text':data[1][1]},
+                {'type':'text', 'text':data[1][2]}
+            ]);   
              
         })  
     }
