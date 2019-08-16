3041f03b270871026c12c3fadb417886c08c57cc
project405 <10546017@ntub.edu.tw>
2019-08-17 05:57:25 +0800

    myApp

--- index.js
+++ index.js
@@ -152,7 +152,7 @@ bot.on('message', function(event) {
                         event.reply([
                             {'type':'text', 'text':data[1][0]},
                             {'type':'text', 'text':data[1][1]},
-                            {'type':'text', 'text':data[1][2]},
+                            {'type':'text', 'text':data[1][2]}
                         ]);   
                     }  
                 })  
