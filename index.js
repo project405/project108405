a4bf29ce7d951d10519e06fda436bedc9a6a88bf
project405 <10546017@ntub.edu.tw>
2019-08-25 16:28:58 +0800

    myApp

--- index.js
+++ index.js
@@ -99,7 +99,11 @@ bot.on('message',async function (event) {
 
         recommend.getFourRecomClassList().then(data =>{
                 console.log("data!!!!!!",data);
-                console.log("movie",data[0]);
+                event.reply({'type':'text', 'text':msgs[0],'text':data[0]},
+                            {'type':'text', 'text':msgs[1],'text':data[1]},
+                            {'type':'text', 'text':msgs[2],'text':data[2]},
+                            {'type':'text', 'text':msgs[3],'text':data[3]}
+                )
         });
 
 
