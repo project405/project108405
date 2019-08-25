da9b6c45a0983e3fb7540aa4552a2d861299eb4a
project405 <10546017@ntub.edu.tw>
2019-08-25 17:10:23 +0800

    myApp

--- index.js
+++ index.js
@@ -112,7 +112,7 @@ bot.on('message', function(event) {
 
                 //準備食物卡片樣式
                 data.forEach(item => {
-                        console.log(item[0])
+                        console.log(item[0].recomHead)
                 //     msg.push({
                 //         "thumbnailImageUrl": "https://tomlin-app-1.herokuapp.com/imgs/" + item.picture,
                 //         "imageBackgroundColor": "#FFFFFF",
