200bd9de385f29d670194cfa3499aa1cb3342819
project405 <10546017@ntub.edu.tw>
2019-08-31 03:31:32 +0800

    myApp

--- index.js
+++ index.js
@@ -174,6 +174,7 @@ bot.on('message', function(event) {
                     "columns": [
                         {
                           "thumbnailImageUrl": "https://weiting.nctu.me/imgs/recommend/movie1.jpg#",
+                          "title": "電影類",
                           "title": data[0][0].recomHead,
                           "text": "movie1年代、導演、演員",
                           "defaultAction": {
