1bd06572cf58963776f457789bfa2464d3c9d10e
project405 <10546017@ntub.edu.tw>
2019-08-31 04:39:20 +0800

    myApp

--- index.js
+++ index.js
@@ -105,10 +105,10 @@ bot.on('postback', function(event) {
     let recom_class = ['movie','music','book','exhibition'];
     // const userId = event.source.userId;
     var recom = recom_class.map(function(item,index){
-        console.log(item)
-        console.log(index)
+        // console.log(item)
+        // console.log(index)
         recommend.getRecomClassList(item).then(d =>{
-            console.log(d[0]);
+            console.log(d);
             // const data = event.postback.data;
             // //存放recommend/movie1.content
             // const recommendData = d[0][0].recomCont;
