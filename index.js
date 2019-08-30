22e2f655365b180f8d72ebbe6720a027d06da9e2
project405 <10546017@ntub.edu.tw>
2019-08-31 02:23:21 +0800

    myApp

--- index.js
+++ index.js
@@ -184,7 +184,7 @@ bot.on('message', function(event) {
                           {
                               "type": "postback",
                               "label": "知道更多",
-                              "data": "action=buy&itemid=111"
+                              "data": "recommend/movie1"
                           },
                           {
                               "type": "postback",
