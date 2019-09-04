722b2b23ea29eda6360de36176322d0ff45e4b39
project405 <10546017@ntub.edu.tw>
2019-09-04 23:34:37 +0800

    myApp

--- index.js
+++ index.js
@@ -236,7 +236,7 @@ bot.on('message', function(event) {
                               {
                                   "type": "postback",
                                   "label": "知道更多",
-                                  "data": data[0][0].recomHead
+                                  "data": 'movie'
                               },
                               {
                                 //-------!需克服收藏資料寫入資料庫的問題
