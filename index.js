2e4f731ae633a0a27ff569fd2c46e4611bc32e74
project405 <10546017@ntub.edu.tw>
2019-09-09 14:44:38 +0800

    myApp

--- index.js
+++ index.js
@@ -275,8 +275,8 @@ bot.on('message', function(event) {
             console.log(data[1])
             event.reply([
                 { type: 'text', text: '時間：' + data[1][0].artiDateTime  + '\n'+ '標題：' + data[1][result[0]].artiHead  + '\n'+ '連結：' + `https://tomlin-app-1.herokuapp.com/article/${data[1][result[0]].articleNum}` },
-                { type: 'text', text: '時間：' + data[1][result[1]].artiDateTime  + '\n'+ '標題：' + data[1][result[1]].artiHead  + '\n'+ '連結：' + `https://tomlin-app-1.herokuapp.com/article/${data[1][result[1]].articleNum}` },
-                { type: 'text', text: '時間：' + data[1][result[2]].artiDateTime  + '\n'+ '標題：' + data[1][result[2]].artiHead  + '\n'+ '連結：' + `https://tomlin-app-1.herokuapp.com/article/${data[1][result[2]].articleNum}` }
+                { type: 'text', text: '時間：' + data[1][1].artiDateTime  + '\n'+ '標題：' + data[1][result[1]].artiHead  + '\n'+ '連結：' + `https://tomlin-app-1.herokuapp.com/article/${data[1][result[1]].articleNum}` },
+                { type: 'text', text: '時間：' + data[1][2].artiDateTime  + '\n'+ '標題：' + data[1][result[2]].artiHead  + '\n'+ '連結：' + `https://tomlin-app-1.herokuapp.com/article/${data[1][result[2]].articleNum}` }
             ]);
 
 
