548f13d66ef39a06d2344f7d065d5971553cf543
project405 <10546017@ntub.edu.tw>
2019-09-09 11:48:47 +0800

    myApp

--- index.js
+++ index.js
@@ -110,9 +110,9 @@ bot.on('postback', function(event) {
             const userId = profile.userId;
             const data = event.postback.data;
             console.log(data)
-            logIn.userLogIn(userId){
-                
-            }
+            // logIn.userLogIn(userId){
+
+            // }
             // if (data == '收藏'){
             //     event.reply({'type':'text', 'text':"https://project108405.herokuapp.com/logIn?"+userId});
             // }
