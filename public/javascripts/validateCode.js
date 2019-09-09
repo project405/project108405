f8030af5896b21006f0aa589569a6b575029b0b3
project405 <10546017@ntub.edu.tw>
2019-09-09 20:13:01 +0800

    adjust

--- public/javascripts/validateCode.js
+++ public/javascripts/validateCode.js
@@ -1,35 +1,55 @@
-var code; //在全局定義驗證碼
-//產生驗證碼
-window.onload = function () {
-    createCode();
-}
 
-function createCode() {
-    code = "";
-    var codeLength = 5; //驗證碼的長度
-    var checkCode = document.getElementById("checkCode");
-    var random = new Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'); //隨機數
+$(document).ready(function() {
+    liff.init(
+        data => {
+            // Now you can call LIFF API
+            const userId = data.context.userId;
+            const accessToken = liff.getAccessToken();
+            console.log("accessToken!!!",accessToken)
+            console.log('ashly is very cute', userId)
+            console.log('getThis',$(this).find('#lineID').val(userId))
+            console.log('whatisit',$(this).find('#lineID').val())
+        },
+        err => {
+            // LIFF initialization failed
+            console.error(err)
+        }
+        );
+})
 
-    for (var i = 0; i < codeLength; i++) { //循環操作
-
-        var charIndex = Math.floor(Math.random() * 36); //取得隨機數的索引     
-        code += random[charIndex]; //根據索引取得隨機數加到code上
-
+    var code; //在全局定義驗證碼
+    //產生驗證碼
+    window.onload = function() {
+        createCode();
     }
-    checkCode.value = code; //把code值賦給驗證碼
-}
-//校驗驗證碼
-function validateCode() {
-    var inputCode = document.getElementById("input").value.toUpperCase(); //取得輸入的驗證碼並轉化為大寫
-    if (inputCode.length <= 0) { //若輸入的驗證碼長度為0
-        alert("請輸入驗證碼！"); //則彈出請輸入驗證碼
-    } else if (inputCode != code) { //若輸入的驗證碼與產生的驗證碼不一致時
-
-        alert("驗證碼輸入錯誤！"); //則彈出驗證碼輸入錯誤    
-        createCode(); //刷新驗證碼
-        return false;
-
-    } else { //輸入正確時   
-        document.getElementById("login").submit();
+    
+    function createCode() {
+        code = "";
+        var codeLength = 5; //驗證碼的長度
+        var checkCode = document.getElementById("checkCode");
+        var random = new Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'); //隨機數
+        for (var i = 0; i < codeLength; i++) { //循環操作
+            var charIndex = Math.floor(Math.random() * 36); //取得隨機數的索引     
+            code += random[charIndex]; //根據索引取得隨機數加到code上
+        }
+        checkCode.value = code; //把code值賦給驗證碼
     }
-}
+    //校驗驗證碼
+    async function validateCode () {
+        if (!bind) {
+            var bind = as.getItem('bind')
+        }
+        .setItem('bind', 'ii')
+        set
+        var inputCode = document.getElementById("input").value.toUpperCase(); //取得輸入的驗證碼並轉化為大寫
+        if (inputCode.length <= 0) { //若輸入的驗證碼長度為0
+            alert("請輸入驗證碼！"); //則彈出請輸入驗證碼
+        } else if (inputCode != code) { //若輸入的驗證碼與產生的驗證碼不一致時
+            alert("驗證碼輸入錯誤！"); //則彈出驗證碼輸入錯誤    
+            createCode(); //刷新驗證碼
+            console.log((document.getElementById("login")))
+            return false;
+        } else { //輸入正確時
+            document.getElementById("login").submit();
+        }
+    }
--- routes/member/userLogin.js
+++ routes/member/userLogin.js
@@ -9,26 +9,58 @@ const member = require('../utility/member');
 router.post('/', function (req, res, next) {
     var memID = req.body.memID;                 //取得帳號
     var memPass = req.body.memPass;     //取得密碼
-    login.userLogin(memID, memPass).then(d => {
-        if (d == null) {
-            req.session.memID = null;
-            req.session.memPass = null;
-            res.write('<head><meta charset="utf-8"/></head>');
-            res.end('<script> alert("登入失敗，尚未註冊帳號。"); history.back();</script>');
+    var lineID = req.body.lineID
+    
+    if (lineID) {   
+        login.userBind(memID, memPass, lineID).then(d => {
+            if (d == null) {
+                req.session.memID = null;
+                req.session.memPass = null;
+                res.write('<head><meta charset="utf-8"/></head>');
+                res.end('<script> alert("綁定失敗。"); </script>');
 
-        } else {
-            req.session.memID = d.memID;
-            req.session.memPass = d.memPass;
-            member.checkAuthority(memID).then(data => {
-                var mydata = [];
-                mydata[0] = data;
-                mydata[1] = memID;
-                console.log(mydata);
-                res.render('memberManage', { items: mydata });
-            })
-            // res.render('memberManage', { name: d.memID });   //導向使用者管理頁面
-        }
-    })
+            } else {
+                console.log('bind err')
+                req.session.memID = d.memID;
+                req.session.memPass = d.memPass;
+                // member.checkAuthority(memID).then(data => {
+                //     var mydata = [];
+                //     mydata[0] = data;
+                //     mydata[1] = memID;
+                //     mydata[2] = lineID;
+                //     console.log(mydata);
+                //     // res.render('memberManage', { items: mydata });
+                // })
+                login.addLineID(memID, lineID).then(data => {
+                    console.log('success')
+                },error => {
+                    console.log(error)
+                })
+                res.render('memberManage', { name: d.memID });   //導向使用者管理頁面
+            }
+        })
+    } else {
+        login.userLogin(memID, memPass).then(d => {
+            if (d == null) {
+                req.session.memID = null;
+                req.session.memPass = null;
+                res.write('<head><meta charset="utf-8"/></head>');
+                res.end('<script> alert("登入失敗，尚未註冊帳號。"); </script>');
+    
+            } else {
+                req.session.memID = d.memID;
+                req.session.memPass = d.memPass;
+                member.checkAuthority(memID).then(data => {
+                    var mydata = [];
+                    mydata[0] = data;
+                    mydata[1] = memID;
+                    console.log(mydata);
+                    // res.render('memberManage', { items: mydata });
+                })
+                res.render('memberManage', { name: d.memID });   //導向使用者管理頁面
+            }
+        })
+    }
 });
 
 module.exports = router;
