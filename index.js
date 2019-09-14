3cbb159620caa073a071fef6509b65649c95efad
project405 <10546017@ntub.edu.tw>
2019-09-15 01:28:22 +0800

    myApp

--- index.js
+++ index.js
@@ -202,22 +202,24 @@ bot.on('postback', function(event) {
 
                 async function test() {
                     await logIn.userJudgeBind(userId).then(d =>{
-                       
-                        console.log("已綁定line，準備寫入資料庫")
-                        if(d[0].lineID == userId){
-                            console.log('memID',d[0].memID)
-                            console.log('userId!!!',userId)
-                            console.log('data!!!',data)
-                            
-                            console.log(typeof(parseInt(data)))
-                            console.log('寫一個收藏進去');
-                            collection.addLineColleRecommend(d[0].memID, parseInt(data)).then(b =>{
-                                console.log(b)
-                            })
-                        }else{
-                            event.reply(myLineTemplate)
-        
-                        }
+                        event.source.profile().then(
+                            function (profile) {
+                                const userId = profile.userId;
+                                console.log("已綁定line，準備寫入資料庫")
+                                if(d[0].lineID == userId){
+                                    console.log('memID',d[0].memID)
+                                    console.log('userId!!!',userId)
+                                    console.log('data!!!',data)
+                                    
+                                    console.log(typeof(parseInt(data)))
+                                    console.log('寫一個收藏進去');
+                                    collection.addLineColleRecommend(d[0].memID, parseInt(data)).then(b =>{
+                                        console.log(b)
+                                    })
+                                }else{
+                                    event.reply(myLineTemplate)
+                            }
+                        })
                     })
                 }
                 
