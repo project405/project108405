cfbfe7b3ab2196f968ce488c7456a02660509a6e
project405 <10546017@ntub.edu.tw>
2019-09-14 21:03:19 +0800

    myApp

--- routes/utility/login.js
+++ routes/utility/login.js
@@ -67,7 +67,7 @@ var userJudgeBind = async function(lineID){
     await sql('SELECT * FROM "member" WHERE "lineID" = $1', [lineID])
         .then((data) => {
             if(data.rows.length > 0){
-                result = data.rows[0][1];
+                result = data.rows[0];
             } else {
                 result = null;
             } 
