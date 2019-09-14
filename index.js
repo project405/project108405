fdb24072ee8b1146486952ef081d204c035e9687
project405 <10546017@ntub.edu.tw>
2019-09-15 01:17:07 +0800

    myApp

--- index.js
+++ index.js
@@ -198,33 +198,34 @@ bot.on('postback', function(event) {
                     }    
                 });
             }else{
-                gotoBind(userId)
-                
+            
+                logIn.userJudgeBind(userId).then(d =>{
+                    console.log(d[0].memID)
+                    console.log("已綁定line，準備寫入資料庫")
+                    if(d[0].lineID == userId){
+                        console.log('memID',d[0].memID)
+                        console.log('userId!!!',userId)
+                        console.log('data!!!',data)
+                        
+                        console.log(typeof(parseInt(data)))
+                        console.log('寫一個收藏進去');
+                        collection.addLineColleRecommend(d[0].memID, parseInt(data)).then(b =>{
+                            console.log(b)
+                        })
+                    }else{
+                        event.reply(myLineTemplate)
+    
+                    }
+                })
                 
             }
-            async function gotoBind(userId){
-                const posts = await logIn.userJudgeBind(userId);
-                console.log(posts);
-            }
-            await logIn.userJudgeBind(userId).then(d =>{
-                console.log(d[0].memID)
-                console.log("已綁定line，準備寫入資料庫")
-                if(d[0].lineID == userId){
-                    console.log('memID',d[0].memID)
-                    console.log('userId!!!',userId)
-                    console.log('data!!!',data)
-                    
-                    console.log(typeof(parseInt(data)))
-                    console.log('寫一個收藏進去');
-                    collection.addLineColleRecommend(d[0].memID, parseInt(data)).then(b =>{
-                        console.log(b)
-                    })
-                }else{
-                    event.reply(myLineTemplate)
-
-                }
-            })
-        
+            
+            // async function validateCode () {
+            //     if (!bind) {
+            //     var bind = await as.getItem('bind')
+            //     }
+            //     .setItem('bind', 'ii')
+            //     set
     });
     // let recom_class = ['movie','music','book','exhibition'];
     // const userId = event.source.userId;
