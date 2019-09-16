f9889193c1b9ec0e2c1221bc600c55981347ab26
project405 <10546017@ntub.edu.tw>
2019-09-16 13:36:17 +0800

    myApp

--- index.js
+++ index.js
@@ -200,20 +200,23 @@ bot.on('postback', function(event) {
             }else{
                 logIn.userJudgeBind(userId).then(d =>{
                     console.log("已綁定line，準備寫入資料庫")
-                    if(d[0].lineID && d[0].lineID == userId){
-                        console.log('memID',d[0].memID)
-                        console.log('userId!!!',userId)
-                        console.log('data~~~~~',data)
-                        
-                        console.log(typeof(parseInt(data)))
-                        console.log('寫一個收藏進去');
-                        collection.addLineColleRecommend(d[0].memID, parseInt(data)).then(b =>{
-                            console.log(b)
-                        })
-                    }else{
-                        event.reply(myLineTemplate)
-    
+                    if(d[0]){
+                        if(d[0].lineID == userId){
+                            console.log('memID',d[0].memID)
+                            console.log('userId!!!',userId)
+                            console.log('data~~~~~',data)
+                            
+                            console.log(typeof(parseInt(data)))
+                            console.log('寫一個收藏進去');
+                            collection.addLineColleRecommend(d[0].memID, parseInt(data)).then(b =>{
+                                console.log(b)
+                            })
+                        }else{
+                            event.reply(myLineTemplate)
+        
+                        }
                     }
+                    
                 })  
             }
             
