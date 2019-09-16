6ec401271ede5873dff2aaedf610950d893c7944
project405 <10546017@ntub.edu.tw>
2019-09-16 13:30:25 +0800

    myApp

--- index.js
+++ index.js
@@ -141,7 +141,7 @@ bot.on('postback', function(event) {
             // }
 
             // }
-            ''
+            
             if (data == 'movie' ||data == 'music' ||data == 'book' || data =='exhibition'){
                 console.log("進到四大推薦")
                 //---------------使用map記得傳入item參數getRecomClassList
