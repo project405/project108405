6a6da2feeb92c5b4022cd029ce6492179ee6aef9
project405 <10546017@ntub.edu.tw>
2019-09-16 13:26:52 +0800

    myApp

--- index.js
+++ index.js
@@ -198,29 +198,23 @@ bot.on('postback', function(event) {
                     }    
                 });
             }else{
-                await test().catch(error => console.log(error.message));
-
-                async function test() {
-                    logIn.userJudgeBind(userId).then(d =>{
+                logIn.userJudgeBind(userId).then(d =>{
+                    console.log("已綁定line，準備寫入資料庫")
+                    if(d[0].lineID == userId){
+                        console.log('memID',d[0].memID)
+                        console.log('userId!!!',userId)
+                        console.log('data~~~~~',data)
                         
-                        console.log("已綁定line，準備寫入資料庫")
-                        if(d[0].lineID == userId){
-                            console.log('memID',d[0].memID)
-                            console.log('userId!!!',userId)
-                            console.log('data!!!',data)
-                            
-                            console.log(typeof(parseInt(data)))
-                            console.log('寫一個收藏進去');
-                            collection.addLineColleRecommend(d[0].memID, parseInt(data)).then(b =>{
-                                console.log(b)
-                            })
-                        }else{
-                            event.reply(myLineTemplate)
-        
-                        }
-                    })
-                }
-                
+                        console.log(typeof(parseInt(data)))
+                        console.log('寫一個收藏進去');
+                        collection.addLineColleRecommend(d[0].memID, parseInt(data)).then(b =>{
+                            console.log(b)
+                        })
+                    }else{
+                        event.reply(myLineTemplate)
+    
+                    }
+                })  
             }
             
             // async function validateCode () {
