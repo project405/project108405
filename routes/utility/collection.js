60578d173ceee22f51ac9a4a297586b5f461c092
project405 <10546017@ntub.edu.tw>
2019-09-22 14:41:27 +0800

    myApp

--- routes/utility/collection.js
+++ routes/utility/collection.js
@@ -1148,7 +1148,7 @@ var addLineColleRecommend = async function (memID, recomNum) {
     var addTime = moment(Date.now()).format("YYYY-MM-DD hh:mm:ss");
     var iscollection;
     var result = 0;
-    console.log(iscollection)
+    console.log("~~~~~~~",iscollection)
     //判斷memberCollection資料庫是否有重複的收藏  
     await sql('SELECT * FROM "memberCollection" WHERE "memID" = $1 and "recomNum" = $2', [memID, recomNum])
             .then((data) => {
