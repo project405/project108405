57c76dd1cc84ebe7aece7a3824f16bee47029f0a
project405 <10546017@ntub.edu.tw>
2019-09-22 15:20:36 +0800

    myApp

--- index.js
+++ index.js
@@ -186,35 +186,27 @@ bot.on('postback', function(event) {
                 });
             }else{
                 logIn.userJudgeBind(userId).then(d =>{
-                   
-
                         if(d[0]){
                             
                             if(d[0].lineID == userId){
                               
-                                
                                 collection.addLineColleRecommend(d[0].memID, parseInt(data)).then(b =>{
                                     console.log(b)
                                     if(b == 0){
                                         event.reply('【重複收藏】\n查看所有收藏\n請至文藝富心官網查看')
                                     }else{
-                                        event.reply('【收藏成功】\n查看所有收藏\n請至文藝富心官網查看')
+                                        event.reply(userName+'【收藏成功】\n查看所有收藏\n請至文藝富心官網查看')
                                     }
-                                })
-                                
+                                })                            
                             }
                             
                         }else{
                             event.reply(myLineTemplate)
         
                         }
-                    
                 })  
-            }
-           
-    });
-  
-    
+            }         
+    });    
 });
 
 
