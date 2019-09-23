7627b0eaec7073658a567c8d9b028371b8a0c655
project405 <10546017@ntub.edu.tw>
2019-09-23 18:08:33 +0800

    myApp

--- index.js
+++ index.js
@@ -260,7 +260,11 @@ bot.on('message', function(event) {
     //-----------本週推薦-----------
 	if(text == '本週推薦'){
         recommend.getFourRecomClassList().then(data =>{
-            new Date(data[0][0].recomDateTime).Format("yyyy/MM/dd")
+            var dt = new Date(data[0][0].recomDateTime);
+            dt.setMinutes(dt.getMinutes() - dt.getTimezoneOffset());
+            var date = dt.toISOString().slice(0, -5).replace(/[T]/g, ' ');
+            console.log(date);
+            // new Date(data[0][0].recomDateTime).Format("yyyy/MM/dd")
             // console.log(dt.toString())
             // new Date(new Date(data[0][0].recomDateTime).toString().split('GMT')[0]+' UTC').toISOString().split('.')[0]
             console.log('@@@@@@@@@@@@',typeof(data[0][0].recomDateTime))
