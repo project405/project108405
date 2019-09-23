5142010bab6aaf1c3397d175ac867b25393d6e52
project405 <10546017@ntub.edu.tw>
2019-09-23 17:58:40 +0800

    myApp

--- index.js
+++ index.js
@@ -260,9 +260,10 @@ bot.on('message', function(event) {
     //-----------本週推薦-----------
 	if(text == '本週推薦'){
         recommend.getFourRecomClassList().then(data =>{
-            new Date(data[0][0].recomDateTime)
+            var dt = new Date(data[0][0].recomDateTime);
+            console.log(dt.toISOString())
             
-            console.log('@@@@@@@@@@@@',data[0][0].recomDateTime)
+            console.log('@@@@@@@@@@@@',typeof(data[0][0].recomDateTime))
 
             var movieNum = data[0][0].recomNum;
             var musicNum = data[1][0].recomNum;
