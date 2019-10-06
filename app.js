809923301f608165795aa72246b2b8fe50e60901
chin <s235068267@gmail.com>
2019-10-06 22:25:06 +0800

    modify logOut bug and report

--- app.js
+++ app.js
@@ -254,29 +254,10 @@ app.get('/auth/google/callback',
 
 app.get('/user/logout', function(req, res){    
     req.logout();        //將使用者資料從session移除
-    req.session.memID = "" ;
+    req.session.memID = undefined ;
+    console.log(req.session.memID);
     res.redirect('/');   //導向登出頁面
 });    
-
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-  });
-  
-  // error handler
-  app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-  });
-
 
 
 // catch 404 and forward to error handler
