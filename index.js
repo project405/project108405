1d76aa6c5f65feb34da96e1bda121eb1453309ce
project405 <10546017@ntub.edu.tw>
2019-10-06 15:02:31 +0800

    myApp

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var express = require('express');
 //增加引用函式
 // const collection = require('./utility/collection');
 const index = require('./routes/utility/index');
-const logIn = require('./routes/utility/login');
+const login = require('./routes/utility/login');
 const collection = require('./routes/utility/collection');
 const recommend = require('./routes/utility/recommend');
 
