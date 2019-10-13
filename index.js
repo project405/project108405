9221c2eb838a66213e3a2a2bbf6d017ad3d84180
project405 <10546017@ntub.edu.tw>
2019-10-13 20:27:17 +0800

    myApp

--- index.js
+++ index.js
@@ -108,8 +108,10 @@ var server = app.listen(process.env.PORT || 3000, function() {
 app.post('/webhook', function (req, res) {
     let allUser = [];
     member.AllMember().then(data => {  
+        data.forEach(item => {
+            allUsers.push(item.userid);
+        });
         
-        allUser = data;
         console.log('allUser@@@@@@@@@@',allUser);
     })   
 
