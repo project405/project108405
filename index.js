e1cd4323baf4470716a88594d035091b79330a44
project405 <10546017@ntub.edu.tw>
2019-10-13 15:21:29 +0800

    myApp

--- index.js
+++ index.js
@@ -157,8 +157,8 @@ app.post('/',cors(corsOptions), function (req, res, next) {
         console.log('req.body!!!!!!!!!!!!!!!!!!!!!!' , req.body.msg);
         let reply_token = req.body.events[0].replyToken
         let msg = req.body.events[0].message.text
-        console.log('reply_token = ' + reply_token);
-        console.log('msgObj = ' , req.body.events[0]);
+        console.log('reply_token = @@@@@@' + reply_token);
+        console.log('msgObj = @@@@@@' , req.body.events[0]);
         // console.log('req.body' , req.body);
 
         reply(reply_token, msg)
