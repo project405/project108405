d0c1f96e3a974cad826571ac683bc699bfcaf332
chin <s235068267@gmail.com>
2019-10-13 10:33:52 +0800

    modify collection error and merge reportV2

--- routes/collection/oneColleArticle.js
+++ routes/collection/oneColleArticle.js
@@ -21,10 +21,8 @@ router.get('/:artiNum', async function (req, res, next) {
         for (var i = 0; i < data[0].length; i++) {
             if (data[0][i].artiCont.match("\\:imgLocation") != null) {
                 console.log("近來囉");
-                for (var j = 0; j < data[11][0].length; j++) {
-                    // artiCont = artiCont.replace("\\:imgLocation", "<div class='wrapperCard card-img-top' style='background-image: url(/userImg/" + req.files[i].filename + "'); border-radius:8px; '></div>");
-                    data[0][i].artiCont = data[0][i].artiCont.replace("\\:imgLocation", "<div class='wrapperCard card-img-top'><img src='/userImg/" + data[11][0][j].imgName + "' style='max-height: 450px; max-width: 70%; cursor: pointer; border-radius: 12px; padding: 0.1em; ' ></div>");
-                    // console.log("data[", 11, "][", j, "]=", data[11][0][j]);
+                for (var j = 0; j < data[6].length; j++) {
+                    data[0][i].artiCont = data[0][i].artiCont.replace("\\:imgLocation", "<div class='wrapperCard card-img-top'><img src='/userImg/" + data[6][j].imgName + "' style='max-height: 450px; max-width: 70%; cursor: pointer; border-radius: 12px; padding: 0.1em; ' ></div>");
                 }
             }
         }
