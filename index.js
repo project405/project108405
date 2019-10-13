f9209020d7e5307b676063a2378c595e0c1a575b
project405 <10546017@ntub.edu.tw>
2019-10-13 18:17:15 +0800

    myApp

--- index.js
+++ index.js
@@ -112,7 +112,7 @@ app.post('/webhook', function (req, res) {
     url: 'https://api.line.me/v2/bot/message/push',
     body: JSON.stringify({
         //to給資料庫有的使用者
-        to: "U58ef73e7aac9254abd484af3ff964c4b",
+        to: "U2251202deb66b8a73da26e53c8399a13",
             messages: [
                 {
                 type: 'text',
