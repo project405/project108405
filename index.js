286299c28e7a0e56752002726a6403dd3e06270d
project405 <10546017@ntub.edu.tw>
2019-10-13 12:33:05 +0800

    myApp

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var express = require('express');
 const app = express();
 var cors = require('cors')
 var corsOptions = {
-    origin: 'https://project-108405.herokuapp.com/',
+    origin: 'http://localhost:3000/',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 app.use(cors())
