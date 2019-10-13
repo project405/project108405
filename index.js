f98df3dbb7b38997bbd13b0acc2e3171217bc245
project405 <10546017@ntub.edu.tw>
2019-10-13 12:45:37 +0800

    myApp

--- index.js
+++ index.js
@@ -150,12 +150,12 @@ app.post('/',cors(corsOptions), function (req, res, next) {
     // Pass to next layer of middleware
     console.log('reqreqreqreqreqreqreqreqreqreqreqreqreqreqreqreqreqreq', req)
 
-        let reply_token = req.body.events[0].replyToken
-        let msg = req.body.events[0].message.text
-        console.log('reply_token = ' + reply_token);
-        console.log('msgObj = ' , req.body.events[0]);
+        // let reply_token = req.body.events[0].replyToken
+        // let msg = req.body.events[0].message.text
+        // console.log('reply_token = ' + reply_token);
+        // console.log('msgObj = ' , req.body.events[0]);
     
-        reply(reply_token, msg)
+        // reply(reply_token, msg)
         res.sendStatus(200)
         next();
 
