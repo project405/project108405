00eebf207048edffbe66a50d537e9044bad7a23e
project405 <10546017@ntub.edu.tw>
2019-10-13 17:18:56 +0800

    myApp

--- index.js
+++ index.js
@@ -173,8 +173,8 @@ app.post('/', function (req, res) {
     },
     url: 'https://api.line.me/v2/bot/message/reply',
     body: JSON.stringify({
-        // replyToken: 'U2251202deb66b8a73da26e53c8399a13',
-        to: "U2251202deb66b8a73da26e53c8399a13",
+        replyToken: 'U2251202deb66b8a73da26e53c8399a13',
+        // to: "U2251202deb66b8a73da26e53c8399a13",
             messages: [
                 {
                 type: 'text',
