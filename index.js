9157ec5b0eed192b4573d3c972f8d9434c05c261
project405 <10546017@ntub.edu.tw>
2019-10-14 10:42:40 +0800

    myApp

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ app.post('/webhook', function (req, res) {
             //Authorization為Channel access token 
             'Authorization': 'Bearer xQw+g1O20RWNkcAoq8UXnPeucNdgBaXKgSv26TQxIUouB1Ld3Y8KpS6vtjWtEldqWl5jRU1Xdp5m0nUUbaKQ7FE+YNVtTQbdGH3D+12qfXFCgk+uXwbgHSbGdmPThSJFvPMqNctqd5jUePtJLTdBggdB04t89/1O/w1cDnyilFU='
         },
-        url: 'https://api.line.me/v2/bot/message/multicast',
+        url: 'https://api.line.me/v2/bot/message/push',
         body: JSON.stringify({
             //to給資料庫有的使用者
             to: allUser,
