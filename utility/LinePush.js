c899ff872a2daddb57664d4dd1ebd0166bbd1662
project405 <10546017@ntub.edu.tw>
2019-10-14 12:45:59 +0800

    myApp

--- utility/LinePush.js
+++ utility/LinePush.js
@@ -26,5 +26,23 @@ var AllMember = async function () {
     return result;
 }
 
+var linebotAddLike = async function () {
+    var result;
+    
+    await sql('SELECT "lineID" FROM "member" where "lineID" is not null')
+        .then((data) => {
+            
+            if (!data.rows) {
+                result = undefined;
+            } else {
+                result = data.rows;
+            }
+        } , (error) => {
+                result = undefined;
+        });
+       
+    return result;
+}
+
 //匯出
 module.exports = {AllMember,linebotAddLike};
