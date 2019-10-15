d0e50bfe992f303c7d9dc709e39b8f67e2a9ea9b
Ashly <ashlyweiting@gmail.com>
2019-10-15 11:07:14 +0800

    recommend guess user prefer done

--- routes/utility/recommend.js
+++ routes/utility/recommend.js
@@ -69,6 +69,8 @@ var getOneRecommend = async function (recomNum, memID) {
     var checkAuthority;
     var result = [];
     var replyImgs = []
+    var guessRecommend = undefined ; //取得猜測使用者可能喜歡的文章
+
 
     // -----------  取得單一推薦文章 --------------
     await sql('SELECT * FROM "recommendListDataView" WHERE "recomNum" = $1', [recomNum])
@@ -183,6 +185,45 @@ var getOneRecommend = async function (recomNum, memID) {
         console.log(error)
     });
 
+    // ----------- 根據該文章的tag去猜測使用者可能喜歡的文章 -----------
+    await sql('SELECT * '+
+                ' FROM "recommend" '+
+                ' WHERE "recomNum" '+
+                    ' IN(SELECT "A"."recomNum" '+
+                    ' FROM (SELECT "recomNum" ,count(*) '+
+                            ' FROM "tagLinkArticle" '+
+                            ' WHERE "tagNum" '+
+                                ' IN (SELECT "tagNum" '+
+                                    ' FROM "tagLinkArticle" '+
+                                    ' WHERE "recomNum" = $1) AND "recomNum" != $1 '+
+                            ' GROUP BY "recomNum" '+
+                            ' ORDER BY "count" DESC , "recomNum" DESC '+
+                            ' LIMIT 3) AS "A")', [recomNum])
+        .then((data) => {
+            if (data.rowCount <= 0) {
+                guessRecommend = undefined ;
+            } else {
+                guessRecommend = data.rows;
+            }   
+        });
+        
+    // ----------- 如果tag沒任何關聯 則隨機取三篇文章 -----------
+    if(guessRecommend == undefined){
+        await sql('SELECT * '+
+            ' FROM "recommend" '+
+            ' ORDER BY random() '+
+            ' LIMIT 3') 
+        .then((data) => {
+                if (!data.rows) {
+                    guessRecommend = undefined ;
+                } else {
+                    
+                    guessRecommend = data.rows;
+                }   
+        });
+    }
+
+
     //取得權限
     await member.checkAuthority(memID).then(data => {
         if (data != undefined) {
@@ -204,6 +245,7 @@ var getOneRecommend = async function (recomNum, memID) {
     result[7] = checkAuthority;
     result[8] = [memID];
     result[9] = replyImgs;
+    result[10] = guessRecommend;
     
     // console.log("QQQQQQQQQQQQQQQQQQQQQQQ",result);
     return result;
