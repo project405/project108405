76562bb168e6d9cc90afbca93e4c7d87fa4dad99
project405 <10546017@ntub.edu.tw>
2019-10-16 00:39:58 +0800

    myApp

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ const member = require('./utility/LinePush');
 const byClassData = require('./utility/index');
 
 byClassData.getIndexData('abc123').then(data =>{
-    console.log('data[10]@@@@@@@@@@@@',data[10])
-    console.log('data[10]@@@@@@@@@@@@',data[10])
+    console.log('data[10]@@@@@@@@@@@@',data[10][2])
+    console.log('data[10]@@@@@@@@@@@@',data[10][3])
     console.log('data[10]@@@@@@@@@@@@',data[10])
 
 
