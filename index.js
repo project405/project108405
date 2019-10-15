cdbb9462335930461d0cddb08bacf5ff6c6c26d4
project405 <10546017@ntub.edu.tw>
2019-10-16 00:47:15 +0800

    myApp

--- index.js
+++ index.js
@@ -25,7 +25,9 @@ const byClassData = require('./utility/index');
 byClassData.getIndexData('abc123').then(data =>{
     // console.log('data[10]@@@@@@@@@@@@',data[10].recomHead)
     // console.log('data[10]@@@@@@@@@@@@',data[10].recomCont)
-    console.log('data[10]@@@@@@@@@@@@',data[10][0])
+    console.log('data[10]@@@@@@@@@@@@',data[10])
+    console.log('data[10][0]@@@@@@@@@@@@',data[10][0])
+
 
 
     // console.log('data[10].byClassData@@@@@@@@@@@@',data[10].byClassData)
