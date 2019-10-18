db0e0ca04935d3c289af175262bccd2b724a68e7
project405 <10546017@ntub.edu.tw>
2019-10-18 19:16:42 +0800

    myApp

--- index.js
+++ index.js
@@ -175,10 +175,12 @@ app.post('/webhook', function (req, res) {
                         // ----------正式line
                         // 'Authorization': 'Bearer xQw+g1O20RWNkcAoq8UXnPeucNdgBaXKgSv26TQxIUouB1Ld3Y8KpS6vtjWtEldqWl5jRU1Xdp5m0nUUbaKQ7FE+YNVtTQbdGH3D+12qfXFCgk+uXwbgHSbGdmPThSJFvPMqNctqd5jUePtJLTdBggdB04t89/1O/w1cDnyilFU='
                     },
-                    url: 'https://api.line.me/v2/bot/message/multicast',
+                    // url: 'https://api.line.me/v2/bot/message/multicast',
+                    url: 'https://api.line.me/v2/bot/message/push',
                     body: JSON.stringify({
                         //to給資料庫有的使用者
-                        to: allUser,
+                        // to: allUser,
+                        to: 'U2251202deb66b8a73da26e53c8399a13',
                             messages: [
                                 {
                                     type: "template",
@@ -201,6 +203,7 @@ app.post('/webhook', function (req, res) {
                                     }
                                 }               
                             ]
+                            
                         })
                 }, function(error, response, body){
                 
