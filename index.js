1c0ba7347ca6fb63be40ae6cc808ede506c66b1f
project405 <10546017@ntub.edu.tw>
2019-10-20 12:00:44 +0800

    myApp

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.post('/webhook',  function (req, res) {
             console.log(allUser)
         });
         byClassData.getIndexData().then(data =>{
-            console.log('%%%%%%%%%%%%%%%%%%%data[10]',bodyParser.json(data[10]))
+            console.log('%%%%%%%%%%%%%%%%%%%data[10]',data[10][0])
             var pushContent = []
             //data為文章
             if(data[10][0].recomHead == undefined){
