03c57d18b496608d8efa2333cf868fd4095a272f
project405 <10546017@ntub.edu.tw>
2019-10-20 15:37:22 +0800

    myApp

--- index.js
+++ index.js
@@ -183,7 +183,7 @@ app.post('/webhook',  function (req, res) {
                                             {  
                                                 "type":"postback",
                                                 "label":"我喜歡",
-                                                "data":"islike"
+                                                "data":postbackData
                                             },
                                             {  
                                                 "type":"postback",
