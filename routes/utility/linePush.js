0cdd4019d5e53b766a9c24b6b5a52dd785556920
project405 <10546017@ntub.edu.tw>
2019-10-20 19:44:59 +0800

    myApp

--- routes/utility/linePush.js
+++ routes/utility/linePush.js
@@ -19,7 +19,7 @@ var AddArticleLike = async function (lineID,artiNum) {
               'FROM "articleLike" '+
               'WHERE "memID" IN (SELECT "memID"  FROM  "member" WHERE "lineID" =  $1 and "artiNum" = $2)', [lineID,artiNum])
         .then((data) => {
-            console.log(typeof(data.rows))
+            console.log(typeof(data.rows) === null )
             //沒有資料
             if(!data.rows){
                 isLike = undefined; 
