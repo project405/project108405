69992796e8d6447d545561ff24787aa7572d0cb9
Ashly <ashlyweiting@gmail.com>
2019-10-21 14:16:32 +0800

    adjust article button position

--- routes/member/login.js
+++ routes/member/login.js
@@ -1,12 +1,8 @@
 var express = require('express');
 var router = express.Router();
-const member = require('../utility/member');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  member.getMood().then((data) => {
-    console.log('loginData', data)
-  }) 
   res.render('login');
 });
 
--- routes/utility/member.js
+++ routes/utility/member.js
@@ -1022,120 +1022,6 @@ var getMemberInfor = async function (memID) {
 
     return result;
 }
-//=========================================
-//---------  getMood() -----------
-//=========================================
-var getMood = async function () {
-    var negativerecom= [];
-    var negativearti = [];
-    var positiverecom = [];
-    var positivearti = [];
-    var temp = []
-    var result = [];
-    var positiveChoose = Math.round(Math.random())
-    var negativeChoose = Math.round(Math.random())
-    async function compare(item, index) {
-        if (item.length != 0 && index <= 1) {
-            if (Object.keys(item[0]).indexOf('artiNum') >= 0 ) {
-                var contentObj =  {
-                    artiNum: item[0].artiNum,
-                    artiHead:  item[0].artiHead,
-                    artiCont:  item[0].artiCont,
-                }
-                result.push(contentObj)
-              } else {
-                // 推薦
-                var contentObj =  {
-                    recomNum: item[0].recomNum,
-                    recomHead:  item[0].recomHead,
-                    recomCont:  item[0].recomCont,
-                }
-                result.push(contentObj)
-              }
-        } else if (item.length != 0 && index >= 2) {
-            if (Object.keys(item[0]).indexOf('artiNum') >= 0 ) {
-                //  文章
-                var contentObj =  {
-                    artiNum: item[0].artiNum,
-                    artiHead:  item[0].artiHead,
-                    artiCont:  item[0].artiCont,
-                }
-                result.push(contentObj)
-              } else {
-                // 推薦
-                var contentObj =  {
-                    recomNum: item[0].recomNum,
-                    recomHead:  item[0].recomHead,
-                    recomCont:  item[0].recomCont,
-                }
-                result.push(contentObj)
-            }
-        }
-    }
-
-    // -----------  取得推薦清單 --------------
-
-    if (negativeChoose == 0) {
-        await sql('SELECT * FROM( SELECT * FROM "recommend" WHERE "analyzeScore" < 0 ORDER BY "negativeWords" DESC, "analyzeScore" DESC LIMIT 5) AS "A" ORDER BY random() LIMIT 1 ')
-        .then((data) => {
-            if (data.rows != undefined) {
-                negativerecom = data.rows
-            } else {
-                negativerecom = undefined
-            }
-        }, (error) => {
-            negativerecom = undefined;
-        });
-    } else {
-        await sql('SELECT * FROM( SELECT * FROM "article" WHERE "analyzeScore" < 0 ORDER BY "negativeWords" DESC, "analyzeScore" DESC LIMIT 5) AS "A" ORDER BY random() LIMIT 1 ')
-        .then((data) => {
-            if (data.rows != undefined) {
-                negativearti = data.rows
-            } else {
-                negativearti = undefined
-            }
-        }, (error) => {
-            negativearti = undefined;
-        });
-    }
-
-    if (positiveChoose == 0) {
-        await sql('SELECT * FROM(SELECT * FROM "recommend" WHERE "analyzeScore" > 0.5 ORDER BY "positiveWords" DESC, "analyzeScore" DESC LIMIT 5) AS "A" ORDER BY random() LIMIT 1 ')
-        .then((data) => {
-            if (data.rows != undefined) {
-                positiverecom = data.rows
-            } else {
-                positiverecom = undefined
-            }
-        }, (error) => {
-            positiverecom = undefined;
-        });
-    } else {
-        await sql('SELECT * FROM( SELECT * FROM "article" WHERE "analyzeScore" > 0.5 ORDER BY "positiveWords" DESC, "analyzeScore" DESC LIMIT 5) AS "A" ORDER BY random() LIMIT 1 ')
-        .then((data) => {
-            if (data.rows != undefined) {
-                positivearti = data.rows
-            } else {
-                positivearti = undefined
-            }
-        }, (error) => {
-            positivearti = undefined;
-        });
-    
-    }
-    
-    temp[0] = negativerecom;
-    temp[1] = negativearti;
-    temp[2] = positiverecom;
-    temp[3] = positivearti;
-    
-    temp.map( async(item, index) => {
-        if (item != '') {
-            await compare(item, index)
-        }
-    })
-    return result;
-}
 
 //匯出
 module.exports = {
@@ -1146,6 +1032,6 @@ module.exports = {
     addRecommendMessLike, delRecommendMessLike,
     report, checkAuthority, editArticle, deleteArticle, deleteRecommend, editReply, deleteReply, 
     memberInformation, getMemberInfor, recommendReplyPost, deleteRecommendReply,
-    editRecommendReply, editRecommend, getMood
+    editRecommendReply, editRecommend
 
 };
