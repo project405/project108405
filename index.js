714eb617226d8a986dfafd246896adef34d63556
project405 <10546017@ntub.edu.tw>
2019-10-21 20:24:54 +0800

    myApp

--- index.js
+++ index.js
@@ -58,7 +58,78 @@ app.post('/webhook',  function (req, res) {
 
     member.artiImg('105').then(data =>{
         console.log(data)
-    })
+
+   
+    request.post({
+        headers: {
+            'content-type' : 'application/json',
+            //Authorization為Channel access token 
+            // ----------測試line
+            'Authorization': 'Bearer QRKiyeWZcixMaO55Yf35KXjZTkrDD70ZAP2gyt8W55aeLgtA75mOVIkOZpruRurKgUgq6ow1+V85huiGRDEBas0Uq57+o4nNREgClY6s+gSg28gC1HNAbELCV7JxGEDlA2bkF8SuWeFNULCG1Z/lwgdB04t89/1O/w1cDnyilFU='
+            // ----------正式line
+            // 'Authorization': 'Bearer xQw+g1O20RWNkcAoq8UXnPeucNdgBaXKgSv26TQxIUouB1Ld3Y8KpS6vtjWtEldqWl5jRU1Xdp5m0nUUbaKQ7FE+YNVtTQbdGH3D+12qfXFCgk+uXwbgHSbGdmPThSJFvPMqNctqd5jUePtJLTdBggdB04t89/1O/w1cDnyilFU='
+        },
+        url: 'https://api.line.me/v2/bot/message/multicast',
+        // url: 'https://api.line.me/v2/bot/message/push',
+        body: JSON.stringify({
+            //to給資料庫有的使用者
+            to: allUser,
+            // to: 'U2251202deb66b8a73da26e53c8399a13',
+                messages: [
+                    {
+                        "type": "template",
+                        "altText": "相信你會喜歡😎",
+                        "template": {
+                          "type": "buttons",
+                          "text": "【文藝富心】小驚喜 🎉\n對於以下推薦有興趣可至文藝富心官網看更多\n立即點選「喜歡」或「不喜歡」讓我們更了解你💞",
+                          "actions": [
+                            {
+                              "type": "uri",
+                              "label": "▶️ 想看更多",
+                              "uri": "https://123.com"
+                            //   "uri": `https://project108405.herokuapp.com/oneRecommend/${d[0][0].recomNum}`
+                            }
+                          ]
+                        }
+                    },
+                    {
+                        type: "template",
+                        altText: "相信你會喜歡😎",
+                        template: {
+                            type: "buttons",
+                            thumbnailImageUrl: data,
+                            imageAspectRatio: "rectangle",
+                            imageSize: "cover",
+                            imageBackgroundColor: "#FFFFFF",
+                            title: '123',
+                            text:'123',
+                            defaultAction: {
+                                "type": "uri",
+                                "label": "View detail",
+                                "uri": "http://example.com/page/123"
+                            },
+                            actions: [
+                                {  
+                                    "type":"postback",
+                                    "label":"我喜歡",
+                                    "data":'123'
+                                },
+                                {  
+                                    "type":"postback",
+                                    "label":"我不喜歡",
+                                    "data":'dislike'
+                                }
+                            ]
+                        }
+                    }                
+                ]
+            })
+    }, function(error, response, body){
+    
+        res.end(body);
+    
+    });
+})
 
     // let allUser = [];
     // member.AllMember().then(data => {  
@@ -194,78 +265,78 @@ app.post('/webhook',  function (req, res) {
     //             });
     //         }
     //         //文章、推薦內容有圖片的推播樣式 
-    //         function linePushPhoto(){
-    //             request.post({
-    //                 headers: {
-    //                     'content-type' : 'application/json',
-    //                     //Authorization為Channel access token 
-    //                     // ----------測試line
-    //                     'Authorization': 'Bearer QRKiyeWZcixMaO55Yf35KXjZTkrDD70ZAP2gyt8W55aeLgtA75mOVIkOZpruRurKgUgq6ow1+V85huiGRDEBas0Uq57+o4nNREgClY6s+gSg28gC1HNAbELCV7JxGEDlA2bkF8SuWeFNULCG1Z/lwgdB04t89/1O/w1cDnyilFU='
-    //                     // ----------正式line
-    //                     // 'Authorization': 'Bearer xQw+g1O20RWNkcAoq8UXnPeucNdgBaXKgSv26TQxIUouB1Ld3Y8KpS6vtjWtEldqWl5jRU1Xdp5m0nUUbaKQ7FE+YNVtTQbdGH3D+12qfXFCgk+uXwbgHSbGdmPThSJFvPMqNctqd5jUePtJLTdBggdB04t89/1O/w1cDnyilFU='
-    //                 },
-    //                 url: 'https://api.line.me/v2/bot/message/multicast',
-    //                 // url: 'https://api.line.me/v2/bot/message/push',
-    //                 body: JSON.stringify({
-    //                     //to給資料庫有的使用者
-    //                     to: allUser,
-    //                     // to: 'U2251202deb66b8a73da26e53c8399a13',
-    //                         messages: [
-    //                             {
-    //                                 "type": "template",
-    //                                 "altText": "相信你會喜歡😎",
-    //                                 "template": {
-    //                                   "type": "buttons",
-    //                                   "text": "【文藝富心】小驚喜 🎉\n對於以下推薦有興趣可至文藝富心官網看更多\n立即點選「喜歡」或「不喜歡」讓我們更了解你💞",
-    //                                   "actions": [
-    //                                     {
-    //                                       "type": "uri",
-    //                                       "label": "▶️ 想看更多",
-    //                                       "uri": "https://123.com"
-    //                                     //   "uri": `https://project108405.herokuapp.com/oneRecommend/${d[0][0].recomNum}`
-    //                                     }
-    //                                   ]
-    //                                 }
-    //                             },
-    //                             {
-    //                                 type: "template",
-    //                                 altText: "相信你會喜歡😎",
-    //                                 template: {
-    //                                     type: "buttons",
-    //                                     thumbnailImageUrl: "https://5b0988e595225.cdn.sohucs.com/images/20190211/7d75600d110246ada740db614d2b10af.jpeg",
-    //                                     imageAspectRatio: "rectangle",
-    //                                     imageSize: "cover",
-    //                                     imageBackgroundColor: "#FFFFFF",
-    //                                     title: `${pushContent[2]}`,
-    //                                     text: `${pushContent[3]}`,
-    //                                     defaultAction: {
-    //                                         "type": "uri",
-    //                                         "label": "View detail",
-    //                                         "uri": "http://example.com/page/123"
-    //                                     },
-    //                                     actions: [
-    //                                         {  
-    //                                             "type":"postback",
-    //                                             "label":"我喜歡",
-    //                                             "data":`${pushContent[0]}`+`${pushContent[1]}`
-    //                                         },
-    //                                         {  
-    //                                             "type":"postback",
-    //                                             "label":"我不喜歡",
-    //                                             "data":'dislike'
-    //                                         }
-    //                                     ]
-    //                                 }
-    //                             }                
-    //                         ]
-    //                     })
-    //             }, function(error, response, body){
+            function linePushPhoto(){
+                request.post({
+                    headers: {
+                        'content-type' : 'application/json',
+                        //Authorization為Channel access token 
+                        // ----------測試line
+                        'Authorization': 'Bearer QRKiyeWZcixMaO55Yf35KXjZTkrDD70ZAP2gyt8W55aeLgtA75mOVIkOZpruRurKgUgq6ow1+V85huiGRDEBas0Uq57+o4nNREgClY6s+gSg28gC1HNAbELCV7JxGEDlA2bkF8SuWeFNULCG1Z/lwgdB04t89/1O/w1cDnyilFU='
+                        // ----------正式line
+                        // 'Authorization': 'Bearer xQw+g1O20RWNkcAoq8UXnPeucNdgBaXKgSv26TQxIUouB1Ld3Y8KpS6vtjWtEldqWl5jRU1Xdp5m0nUUbaKQ7FE+YNVtTQbdGH3D+12qfXFCgk+uXwbgHSbGdmPThSJFvPMqNctqd5jUePtJLTdBggdB04t89/1O/w1cDnyilFU='
+                    },
+                    url: 'https://api.line.me/v2/bot/message/multicast',
+                    // url: 'https://api.line.me/v2/bot/message/push',
+                    body: JSON.stringify({
+                        //to給資料庫有的使用者
+                        to: allUser,
+                        // to: 'U2251202deb66b8a73da26e53c8399a13',
+                            messages: [
+                                {
+                                    "type": "template",
+                                    "altText": "相信你會喜歡😎",
+                                    "template": {
+                                      "type": "buttons",
+                                      "text": "【文藝富心】小驚喜 🎉\n對於以下推薦有興趣可至文藝富心官網看更多\n立即點選「喜歡」或「不喜歡」讓我們更了解你💞",
+                                      "actions": [
+                                        {
+                                          "type": "uri",
+                                          "label": "▶️ 想看更多",
+                                          "uri": "https://123.com"
+                                        //   "uri": `https://project108405.herokuapp.com/oneRecommend/${d[0][0].recomNum}`
+                                        }
+                                      ]
+                                    }
+                                },
+                                {
+                                    type: "template",
+                                    altText: "相信你會喜歡😎",
+                                    template: {
+                                        type: "buttons",
+                                        thumbnailImageUrl: "https://5b0988e595225.cdn.sohucs.com/images/20190211/7d75600d110246ada740db614d2b10af.jpeg",
+                                        imageAspectRatio: "rectangle",
+                                        imageSize: "cover",
+                                        imageBackgroundColor: "#FFFFFF",
+                                        title: `${pushContent[2]}`,
+                                        text: `${pushContent[3]}`,
+                                        defaultAction: {
+                                            "type": "uri",
+                                            "label": "View detail",
+                                            "uri": "http://example.com/page/123"
+                                        },
+                                        actions: [
+                                            {  
+                                                "type":"postback",
+                                                "label":"我喜歡",
+                                                "data":`${pushContent[0]}`+`${pushContent[1]}`
+                                            },
+                                            {  
+                                                "type":"postback",
+                                                "label":"我不喜歡",
+                                                "data":'dislike'
+                                            }
+                                        ]
+                                    }
+                                }                
+                            ]
+                        })
+                }, function(error, response, body){
                 
-    //                 res.end(body);
+                    res.end(body);
                 
-    //             });
+                });
                
-    //         }
+            }
     
     //     })    
     // })      
