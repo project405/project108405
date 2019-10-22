8e049bb268ee284867050f75aa9a79c3ea71797b
project405 <10546017@ntub.edu.tw>
2019-10-23 00:29:11 +0800

    myApp

--- index.js
+++ index.js
@@ -153,7 +153,7 @@ app.post('/webhook',  function (req, res) {
               const response = await request.json();
               console.log(response);
             } catch (e) {
-              throw new Error(e);
+            //   throw new Error(e);
             }
             
             
