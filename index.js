355f61111d78604d7cdaa958abb506b6113380d1
project405 <10546017@ntub.edu.tw>
2019-10-23 01:16:10 +0800

    myApp

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ app.post('/webhook',  function (req, res) {
     member.artiImg('105').then(data =>{
         // console.log(data[0].imgName)
         // console.log(typeof(data[0].imgName))
-        let test = []
+        const test = []
         var img = data[0].imgName.replace('data:image/jpeg;base64,', '');
         var apiUrl = 'https://api.imgur.com/3/image';
         var apiKey = '8b8755d8a1c4ace';
