0f49f6740cfae71051811b703e672bdef2bf96c1
10546004 <10546004@ntub.edu.tw>
2019-10-26 00:41:35 +0800

    init ppt

--- mydb/DeleteView.sql
+++
@@ -1,8 +0,0 @@
--- 刪除 view
--- article
-DROP VIEW IF EXISTS "public"."articleTagView";
-DROP VIEW IF EXISTS "public"."articleListDataView";
-
--- recommend 
-DROP VIEW IF EXISTS "public"."recommendTagView";
-DROP VIEW IF EXISTS "public"."recommendListDataView";
