efc7842ce77fde34e6ef75fad2e75177e3b3cbb9
project405 <10546017@ntub.edu.tw>
2019-10-26 10:18:17 +0800

    myApp

--- utility/LinePush.js
+++ utility/LinePush.js
@@ -2,7 +2,9 @@
 
 //引用操作資料庫的物件
 const sql = require('./asyncDB');
-const moment = require('moment');
+// const moment = require('moment');
+const fetch = require("node-fetch");
+
 
 
 //==============================
@@ -27,6 +29,9 @@ var AllMember = async function () {
     return result;
 }
 
+//==============================
+//-------- 抓取推薦圖片 --------
+//==============================  
 
 var recomImg = async function (recomNum) {
     var result;
@@ -43,6 +48,10 @@ var recomImg = async function (recomNum) {
     return result;     
 }
 
+//==============================
+//-------- 抓取文章圖片 --------
+//==============================  
+
 var artiImg = async function (artiNum) {
     var result;
     await sql('SELECT "imgName" FROM "image" WHERE "artiNum" = $1 LIMIT 1', [artiNum])
@@ -58,6 +67,26 @@ var artiImg = async function (artiNum) {
     return result;
 }
 
+//==============================
+//-------- 圖片解碼base64 --------
+//==============================        
+var Imgur = async function (img) {
+    const url = 'https://api.imgur.com/3/image',
+        request = await fetch(url, {
+            method: 'POST',
+            headers: {
+              "Content-Type": "application/json", 
+              "Authorization": 'Client-ID 8b8755d8a1c4ace',
+            },
+            dataType:"json" ,
+            body: img
+            
+        });
+const response = await request.json();
+return response.data.link;
+};
+
+
 
 //匯出
-module.exports = {AllMember,recomImg,artiImg};
+module.exports = {AllMember,recomImg,artiImg,Imgur};
