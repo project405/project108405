28357228de0556d881ac528ce9ba40a578d9355b
project405 <10546017@ntub.edu.tw>
2019-10-26 11:11:38 +0800

    myApp

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ app.post('/webhook',  function (req, res) {
                 if (data[10][0].artiCont.match("\\:imgLocation") != null){
                     pushContent.push(data[10][0].artiCont.replace(/\\:imgLocation/ig, "img"));
                     member.artiImg(data[10][0].artiNum).then(secondData =>{
-                        console.log('~~~secondData',secondData)
+                        // console.log('~~~secondData',secondData)
                         var img = secondData[0].imgName.replace('data:image/jpeg;base64,', '');
                         member.Imgur(img).then(thirdData => {  
                             console.log(thirdData)
