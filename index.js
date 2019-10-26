556d5f1e438c8fe43a4a1f6a7503bf384dcfd80c
project405 <10546017@ntub.edu.tw>
2019-10-26 18:50:05 +0800

    myApp

--- index.js
+++ index.js
@@ -84,6 +84,7 @@ app.post('/webhook',  function (req, res) {
                     var a ;
                     // pushContent.push(articleCont.replace(/\\:imgLocation/ig, ' '));
                     a = articleCont.replace(/\\:imgLocation/ig, ' ');
+                    console.log("長度！！",a)
                     if (a.length >= 60){
                         pushContent.push(a.slice(0,61)+'...')
                     }else{
@@ -121,6 +122,7 @@ app.post('/webhook',  function (req, res) {
                     var a ;
                     // pushContent.push(articleCont.replace(/\\:imgLocation/ig, ' '));
                     a = recommendCont.replace(/\\:imgLocation/ig, ' ');
+                    console.log("長度！！",a)
                     if (a.length >= 60){
                         pushContent.push(a.slice(0,61)+'...')
                     }else{
