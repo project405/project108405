1334ed1f11cadce5f25d3e8765fdc3a84816d6c7
project405 <10546017@ntub.edu.tw>
2019-10-26 12:55:53 +0800

    myApp

--- index.js
+++ index.js
@@ -219,7 +219,7 @@ app.post('/webhook',  function (req, res) {
                 });
             }
             //文章、推薦內容有圖片的推播樣式 
-            function linePushPhoto(){
+            function linePushPhoto(pushContent){
                 request.post({
                     headers: {
                         'content-type' : 'application/json',
