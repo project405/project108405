f95ca85d2636463ae8f7541bcad65e759dba7660
project405 <10546017@ntub.edu.tw>
2019-10-26 20:17:10 +0800

    myApp

--- index.js
+++ index.js
@@ -255,7 +255,7 @@ app.post('/webhook',  function (req, res) {
                                     altText: "相信你會喜歡😎",
                                     template: {
                                         type: "buttons",
-                                        thumbnailImageUrl: `${s}`,
+                                        thumbnailImageUrl: `${s[0]}`,
                                         imageAspectRatio: "rectangle",
                                         imageSize: "cover",
                                         imageBackgroundColor: "#FFFFFF",
