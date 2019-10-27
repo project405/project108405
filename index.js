393d6903d64e27286e6ae6627d3248329007ff81
project405 <10546017@ntub.edu.tw>
2019-10-27 10:37:55 +0800

    myApp

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ app.post('/webhook',  function (req, res) {
                         var img = secondData[0].imgName.replace('data:image/jpeg;base64,', '');
                         if(img){
                             LinePush.Imgur(img).then(thirdData => {  
-                                pushImg.push(thirdData);
+                                this.pushImg.push(thirdData);
                                                      
                                 // linePushPhoto(pushImg[0]);
                                 linePushPhoto();
@@ -135,7 +135,7 @@ app.post('/webhook',  function (req, res) {
                         var img = secondData[0].imgName.replace('data:image/jpeg;base64,', '');
                         if(img){
                             LinePush.Imgur(img).then(thirdData => {  
-                                pushImg.push(thirdData);
+                                this.pushImg.push(thirdData);
                                                      
                                 // linePushPhoto(pushImg[0]);
                                 linePushPhoto();
