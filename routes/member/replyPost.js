50a9b950b86b75916549f5c6b216106971b9cbb1
Ashly <ashlyweiting@gmail.com>
2019-10-28 13:07:22 +0800

    delete image save local code

--- routes/member/replyPost.js
+++ routes/member/replyPost.js
@@ -6,42 +6,11 @@ const moment = require('moment');
 const multer = require('multer');
 const member = require('../utility/member');
 
-var maxSize = 1024 * 1024; //設定最大上傳容量
-var imgName; //紀錄檔案名稱(不含副檔名)
-var imgType; //記錄檔案副檔名
-var buf; //將檔案名稱做base64編碼
 var isRender = true; //判斷頁面是否有回傳過
 
-var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'public/userImg/replyImg');
-    },
-    filename: function (req, file, cb) {
-        imgName = file.originalname.substring(0, file.originalname.lastIndexOf("."));
-        imgType = file.originalname.substring(file.originalname.lastIndexOf("."));
-        buf = Buffer.from(imgName, 'ascii');
-        buf = buf.toString('base64');
-        //檔名非法字元\/:*?<>|"
-        buf = buf.replace('\\', '');
-        buf = buf.replace('/', '');
-        buf = buf.replace(':', '');
-        buf = buf.replace('*', '');
-        buf = buf.replace('?', '');
-        buf = buf.replace('<', '');
-        buf = buf.replace('>', '');
-        buf = buf.replace('|', '');
-        buf = buf.replace('\"', '');
-        buf += imgType;
-        imgName = Date.now() + "--" + buf;
-        //設定檔案名稱並儲存
-        cb(null, imgName);
-    }
-})
 var upload = multer({
-    storage: storage
+    storage: undefined
 })
-
-
 //post請求
 router.post('/', upload.array('userImg', 100), function (req, res, next) {
     var memID;
@@ -76,45 +45,14 @@ router.post('/', upload.array('userImg', 100), function (req, res, next) {
     }
     // tag
     if (memID == undefined || memID == null) {
-        if (req.body.userImg != 'undefined') {
-            for (var i = 0; i < imgData.length; i++) {
-                fs.unlinkSync('public/userImg/replyImg/' + imgData[i]); //刪除檔案
-            }
-        }
         res.send("請進行登入");
     } else {
-        if (typeof (req.file) != 'undefined') {
-            //如果檔案超過限制大小
-            if (req.file.size > maxSize) {
-                isRender = false;
-                for (var i = 0; i < imgData.length; i++) {
-                    fs.unlinkSync('public/userImg/replyImg/' + imgData[i]); //刪除檔案
-                }
-                res.send("圖片過大，僅接受1M以下的圖片");
-            }
-            //如果檔案類型不符合規定
-            if ((imgType != '.png' && imgType != '.jpg' && imgType != '.jpeg' && imgType != '.jfif') && isRender) {
-                isRender = false;
-                for (var i = 0; i < imgData.length; i++) {
-                    fs.unlinkSync('public/userImg/replyImg/' + imgData[i]); //刪除檔案
-                }
-                res.send("只能上傳.jpg , .png , .jpeg , .jfif 類型的檔案");
-            }
-        }
         if (isRender) {
-            if (req.body.userImg == 'undefined') {
-                for (var i = 0; i < imgData.length; i++) {
-                    fs.unlinkSync('public/userImg/replyImg/' + imgData[i]); //刪除檔案
-                }
-            }
             if (editReply) {
                 member.editReply(artiNum, memID, replyCont, postDateTime, req.body.base64Index, analyzeScore, positiveWords, negativeWords, swearWords, req.body.artiMessNum, req.body.remainImg, req.body.score2).then(data => {
                     if (data == 1) {
                         res.send("編輯留言成功");
                     } else {
-                        for (var i = 0; i < imgData.length; i++) {
-                            fs.unlinkSync('public/userImg/replyImg/' + imgData[i]); //刪除檔案
-                        }
                         res.send("編輯留言失敗");
                     }
                 })
@@ -123,17 +61,11 @@ router.post('/', upload.array('userImg', 100), function (req, res, next) {
                     if (data == 0) {
                         res.send("留言成功");
                     } else {
-                        for (var i = 0; i < imgData.length; i++) {
-                            fs.unlinkSync('public/userImg/replyImg/' + imgData[i]); //刪除檔案
-                        }
                         res.send("留言失敗");
                     }
                 })
             }
         } else {
-            for (var i = 0; i < imgData.length; i++) {
-                fs.unlinkSync('public/userImg/replyImg/' + imgData[i]); //刪除檔案
-            }
             res.send("留言失敗");
         }
     }
--- routes/recommend/post.js
+++ routes/recommend/post.js
@@ -7,39 +7,10 @@ const multer = require('multer');
 const member = require('../utility/member');
 
 //post
-var maxSize = 1024 * 1024; //設定最大上傳容量
-var imgName; //紀錄檔案名稱(不含副檔名)
-var imgType; //記錄檔案副檔名
-var buf; //將檔案名稱做base64編碼
 var isRender = true; //判斷頁面是否有回傳過
 
-var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'public/imgs/recommend');
-    },
-    filename: function (req, file, cb) {
-        imgName = file.originalname.substring(0, file.originalname.lastIndexOf("."));
-        imgType = file.originalname.substring(file.originalname.lastIndexOf("."));
-        buf = Buffer.from(imgName, 'ascii');
-        buf = buf.toString('base64');
-        //檔名非法字元\/:*?<>|"
-        buf = buf.replace('\\', '');
-        buf = buf.replace('/', '');
-        buf = buf.replace(':', '');
-        buf = buf.replace('*', '');
-        buf = buf.replace('?', '');
-        buf = buf.replace('<', '');
-        buf = buf.replace('>', '');
-        buf = buf.replace('|', '');
-        buf = buf.replace('\"', '');
-        buf += imgType;
-        imgName = Date.now() + "--" + buf;
-        //設定檔案名稱並儲存
-        cb(null, imgName);
-    }
-})
 var upload = multer({
-    storage: storage
+    storage: undefined
 })
 
 //post請求
@@ -68,62 +39,22 @@ router.post('/', upload.array('userImg', 100), function (req, res, next) {
       memID = req.session.passport.user.id;
   }
 
-  for (var i in req.files) {
-      imgData.push(req.files[i].filename);
-  }
-
   // tag
   if (req.body.tag != '') {
       tagData = req.body.tag.split(",");
   }
   if (memID == undefined) {
-      if (req.body.userImg != 'undefined') {
-          for (var i = 0; i < imgData.length; i++) {
-              fs.unlinkSync('public/imgs/recommend/' + imgData[i]); //刪除檔案
-          }
-      }
       res.send("請進行登入");
   } else {
-      if (typeof (req.file) != 'undefined') {
-          //如果檔案超過限制大小
-          if (req.file.size > maxSize) {
-              isRender = false;
-              for (var i = 0; i < imgData.length; i++) {
-                  fs.unlinkSync('public/imgs/recommend/' + imgData[i]); //刪除檔案
-              }
-              res.send("圖片過大，僅接受1M以下的圖片");
-          }
-          //如果檔案類型不符合規定
-          if ((imgType != '.png' && imgType != '.jpg' && imgType != '.jpeg' && imgType != '.jfif') && isRender) {
-              isRender = false;
-              for (var i = 0; i < imgData.length; i++) {
-                  fs.unlinkSync('public/imgs/recommend/' + imgData[i]); //刪除檔案
-              }
-              res.send("只能上傳.jpg , .png , .jpeg , .jfif 類型的檔案");
-          }
-      }
       if (isRender) {
           if (recomHead == 'undefined' || recomCont == '') {
-              if (req.body.userImg != 'undefined') {
-                  for (var i = 0; i < imgData.length; i++) {
-                      fs.unlinkSync('public/imgs/recommend/' + imgData[i]); //刪除檔案
-                  }
-              }
               res.send("標題及內容不可為空，請重新輸入");
           } else {
-              if (req.body.userImg == 'undefined') {
-                  for (var i = 0; i < imgData.length; i++) {
-                      fs.unlinkSync('public/imgs/recommend/' + imgData[i]); //刪除檔案
-                  }
-              }
               if (recomNum) {
                 member.editRecommend(memID, recomHead, recomCont, recomClass, req.body.base64Index, tagData, analyzeScore, positiveWords, negativeWords, recomNum, postDateTime, req.body.remainImg, req.body.score2).then(data => {
                     if (data == 1) {
                         res.send("編輯成功");
                     } else {
-                        for (var i = 0; i < imgData.length; i++) {
-                            fs.unlinkSync('public/imgs/recommend/' + imgData[i]); //刪除檔案
-                        }
                         res.send("編輯失敗");
                     }
                 })
@@ -132,18 +63,12 @@ router.post('/', upload.array('userImg', 100), function (req, res, next) {
                       if (data == 0) {
                           res.send("發文成功");
                       } else {
-                          for (var i = 0; i < imgData.length; i++) {
-                              fs.unlinkSync('public/imgs/recommend/' + imgData[i]); //刪除檔案
-                          }
                           res.send("發文失敗");
                       }
                   })
               }
           }
       } else {
-          for (var i = 0; i < imgData.length; i++) {
-              fs.unlinkSync('public/imgs/recommend/' + imgData[i]); //刪除檔案
-          }
           res.send("發文失敗");
       }
   }
