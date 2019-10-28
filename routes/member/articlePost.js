53b992112ca24b669b0b45588d79e016c6c8ce5c
chin <s235068267@gmail.com>
2019-10-28 19:18:04 +0800

    delete console.log

--- routes/member/articlePost.js
+++ routes/member/articlePost.js
@@ -15,7 +15,6 @@ var upload = multer({
 
 //post請求
 router.post('/', upload.array('userImg', 100), function (req, res, next) {
-    console.log('articlepostData!@@@@@@@@@@@@@@@@@@@@@@@@', memID, artiHead, artiCont, artiClass, postDateTime, req.body.base64Index, tagData, analyzeScore, positiveWords, negativeWords, swearWords, req.body.score2)
     var memID;
     var artiHead = req.body.artiHead;
     var artiCont = req.body.artiCont;
@@ -28,6 +27,8 @@ router.post('/', upload.array('userImg', 100), function (req, res, next) {
     var postDateTime = moment(Date().now).format("YYYY-MM-DD HH:mm:ss");
     var tagData = [];
     var imgData = [];
+    console.log("近來後端了");
+    // console.log('articlepostData!@@@@@@@@@@@@@@@@@@@@@@@@', memID, artiHead, artiCont, artiClass, postDateTime, req.body.base64Index, tagData, analyzeScore, positiveWords, negativeWords, swearWords, req.body.score2)
     //將所有換行符號替代成<br> 
     artiCont = artiCont.replace(/\n/g, "<br>");
 
