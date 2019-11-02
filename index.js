359853d7a3b8776786b1daca2a99737cb857e33f
project405 <10546017@ntub.edu.tw>
2019-11-02 14:25:42 +0800

    myApp

--- index.js
+++ index.js
@@ -449,7 +449,7 @@ bot.on('message', function(event) {
                 recommendDateTime.push(item.recomDateTime);
                 linePush.recomImg(item.recomNum).then(SecondData =>{
                     console.log('SecondData[0].imgName',SecondData[0].imgName)
-                    if(SecondData[0].length > 0){
+                    if(SecondData.length > 0){
                         var img = SecondData[0].imgName.replace('data:image/jpeg;base64,', '');
                         // recommendImg.push(SecondData)
                         linePush.Imgur(img).then(ThirdData =>{
@@ -461,8 +461,9 @@ bot.on('message', function(event) {
                     }
                     
                 }) 
+                console.log('recommendImg',recommendImg)
+
             });
-            console.log('recommendImg',recommendImg)
          
 
 
