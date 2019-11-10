aa40e2346781a0d84b0764e334b9f4e0d6da1b30
project405 <10546017@ntub.edu.tw>
2019-11-10 20:50:09 +0800

    myApp

--- index.js
+++ index.js
@@ -255,7 +255,8 @@ app.post('/webhook',  function (req, res) {
                                     altText: "相信你會喜歡😎",
                                     template: {
                                         type: "buttons",
-                                        thumbnailImageUrl: `${pushImg[0]}`,
+                                        // thumbnailImageUrl: `${pushImg[0]}`,
+                                        thumbnailImageUrl: `http://i.imgur.com/Z1ANnd0.jpg`,
                                         imageAspectRatio: "rectangle",
                                         imageSize: "cover",
                                         imageBackgroundColor: "#FFFFFF",
