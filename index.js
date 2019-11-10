8060f1deae00cb3a5a9f214fbddb14449664e12a
project405 <10546017@ntub.edu.tw>
2019-11-10 21:51:02 +0800

    myApp

--- index.js
+++ index.js
@@ -622,7 +622,7 @@ bot.on('message', function(event) {
             let recommendClass = []  
             let recommendHead = []  
             let recommendDateTime = []  
-            let recommendImg = ['','','',''];
+            let recommendImg = [];
             let checkImg = [];
             
             data[0].forEach(async(item, index) => {
