12e4ecce69ed70c2186c2b347a105002cfe55d13
10546004 <10546004@ntub.edu.tw>
2019-11-13 00:15:33 +0800

    index add sentiment section

--- routes/index.js
+++ routes/index.js
@@ -48,20 +48,16 @@ router.get('/', function (req, res, next) {
         
         // 將正向文章字串替換成圖片
         for (var i = 0; i < data[6].length; i++) {
-            if (data[6][i].artiCont.match("\\:imgLocation") != null) {
-                for (var j = 0; j < data[8].length; j++) {
-                    data[6][i].artiCont = data[6][i].artiCont.replace("\\:imgLocation", "<img class='sentimentImg'  src='" + data[8][j].imgName + "'</div>");
-                }
-            }
+            data[6][i].artiCont = data[6][i].artiCont.replace(/\n/g,' ').replace(/\r/g,' ').replace(/<br>/g,' ').replace(/\\:imgLocation/g, " "); 
+            data[6][i].artiCont = data[6][i].artiCont.length > 300 ? `${data[6][i].artiCont.substring(0,300)}...` : data[6][i].artiCont
+            console.log(data[6][i].artiCont.length)
         }
 
         // 將負向文章字串替換成圖片
         for (var i = 0; i < data[7].length; i++) {
-            if (data[7][i].artiCont.match("\\:imgLocation") != null) {
-                for (var j = 0; j < data[9].length; j++) {
-                    data[7][i].artiCont = data[7][i].artiCont.replace("\\:imgLocation", "<img class='sentimentImg' src='" + data[9][j].imgName + "'</div>");
-                }
-            }
+            data[7][i].artiCont = data[7][i].artiCont.replace(/\n/g,' ').replace(/\r/g,' ').replace(/<br>/g,' ').replace(/\\:imgLocation/g, " "); 
+            data[7][i].artiCont = data[7][i].artiCont.length > 300 ? `${data[7][i].artiCont.substring(0,300)}...` : data[7][i].artiCont
+            console.log(data[7][i].artiCont.length)
         }
         
         if (data == null) {
