8d324c67561a6de76b728d557d02daf5c577f165
Ashly <ashlyweiting@gmail.com>
2019-12-07 16:31:47 +0800

    one special column layout done

--- routes/specialColumn/specialColumn.js
+++ routes/specialColumn/specialColumn.js
@@ -4,23 +4,14 @@ const member = require('../utility/member');
 
 //接收GET請求
 router.get('/', function(req, res, next) {
- 	var memID;
 	//判斷是使用哪種方式登入
-	if (req.session.memID == undefined && req.session.passport == undefined) {
-		res.render('login');
-	} else if (req.session.memID != undefined && req.session.passport == undefined) {
-		memID = req.session.memID;
-	} else if (req.session.memID == undefined && req.session.passport != undefined) {
-		memID = req.session.passport.user.id;
-  }
-	
-	member.checkAuthority(memID).then(data => {
-		if (data == 'SYSOP') {
-      		res.render('specialColumn');
-		} else {
-			res.render("login");
-		}
-	})
+// 	if (req.session.memID == undefined && req.session.passport == undefined) {
+// 	} else if (req.session.memID != undefined && req.session.passport == undefined) {
+// 		memID = req.session.memID;
+// 	} else if (req.session.memID == undefined && req.session.passport != undefined) {
+// 		memID = req.session.passport.user.id;
+//   }
+  res.render('specialColumn');
 });
 
 module.exports = router;
