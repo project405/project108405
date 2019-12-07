ae00edb400325effb97799033c2a2180b4b0f7bc
Ashly <ashlyweiting@gmail.com>
2019-12-07 17:30:36 +0800

    activity list layout done

--- routes/activity/activityList.js
+++ routes/activity/activityList.js
@@ -4,47 +4,8 @@ var router = express.Router();
 const article = require('../utility/article');
 
 //接收GET請求
-router.get('/:artiListNum', function (req, res, next) {
-    var artiListNum = req.params.artiListNum;   //取出參數
-    var memID;
-
-    //判斷是使用哪種方式登入
-    if (req.session.memID != undefined && req.session.passport == undefined) {
-        memID = req.session.memID;
-    } else if (req.session.memID == undefined && req.session.passport != undefined) {
-        memID = req.session.passport.user.id;
-    }
-
-    article.getArticleListPagination(memID, artiListNum).then(data => {
-        data[6][0].count = Math.ceil(data[6][0].count / 10) 
-        data[6][0].count = data[6][0].count == 0 ? 1 : data[6][0].count
-        data[0].map((item) => {
-            item.artiCont = item.artiCont.replace(/\n/g,' ').replace(/\r/g,' ').replace(/<br>/g,' ').replace(/\\:imgLocation/g, " ");
-            console.log(item.deadline)
-            // switch(item.deadline) {
-            //     case 'book':
-            //         item.artiClass = '書籍'
-            //         break;
-            //     case 'movie':
-            //         item.artiClass = '電影'
-            //         break;
-            //     case 'music':
-            //         item.artiClass = '音樂'
-            //         break;
-            //     case 'exhibition':
-            //         item.artiClass = '展覽'
-            //         break;
-            // }
-        })
-        
-        if (data == null) {
-            res.render('error');  //導向錯誤頁面
-        } else if (data.length > 0) {
-            res.render('activityList', { items: data });  //將資料傳給顯示頁面
-        } else {
-            res.render('notFound');  //導向找不到頁面
-        }
-    })
+router.get('/', function (req, res, next) {
+    res.render('activityList');  //將資料傳給顯示頁面
 });
 
 module.exports = router;
